b549920ac6e42cdd6450c366484ab9b3bd2be5a9
Lucas Russo <lerwys@gmail.com>
2012-10-09 17:23:52 -0300

    wb_fmc150/sim/*: new simulation files

---
+++ hdl/modules/custom_wishbone/wb_fmc150/sim/Manifest.py
@@ -0,0 +1,5 @@
+files = ["ads62p49_init_mem.vhd", "amc7823_init_mem.vhd", "cdce72010_init_mem_ext.vhd",
+                 "cdce72010_init_mem_int.vhd", "dac3283_init_mem.vhd",
+#          "ads62p49_init_mem.mif", "amc7823_init_mem.mif", "cdce72010_init_mem_ext.mif",
+#                 "cdce72010_init_mem_int.mif", "dac3283_init_mem.mif"
+];             
