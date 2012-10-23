0046a3010e104c0a15c51a8a170c1d863c1b341a
berkenbrock <jose.berkenbrock@lnls.br>
2012-10-23 14:12:23 -0200

    hdl/Manifest.py: add rffe module in top manifest

--- hdl/Manifest.py
+++ hdl/Manifest.py
@@ -3,9 +3,10 @@
 modules = { "local": [
                 "modules/custom_wishbone",
                 "modules/custom_common",
+                "modules/rffe_top",
                 "ip_cores/general-cores",
 #                "ip_cores/etherbone-core",
                 "platform/virtex6/chipscope"]
-#			"git" : [
-#			]
-		};
+#     "git" : [
+#     ]
+    };
