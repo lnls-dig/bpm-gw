91adb029e33ae92f02408505c3c0d229b5ee72ab
Lucas Russo <lerwys@gmail.com>
2012-12-12 16:16:44 -0200

    syn/dbe_bpm_ebone/*: new synthesis folder for ebone testing

---
+++ hdl/syn/dbe_bpm_ebone/Manifest.py
@@ -0,0 +1,10 @@
+target = "xilinx"
+action = "synthesis"
+
+syn_device = "xc6vlx240t"
+syn_grade = "-1"
+syn_package = "ff1156"
+syn_top = "dbe_bpm_ebone.vhd"
+syn_project = "dbe_bpm_ebone.xise"
+
+modules = { "local" : [ "../../top/ml_605/dbe_bpm_ebone" ] };
