88ef40ee7edddcb4ee36125fbb16b823d030f45f
Lucas Russo <lerwys@gmail.com>
2013-02-17 17:11:18 -0300

    emb-sw/*/ethmac_adapt.(c|h): add support for ethmac adapter module

---
+++ embedded-sw/include/ethmac_adapt.h
@@ -0,0 +1,24 @@
+#ifndef _ETHMAC_ADAPT_H_
+#define _ETHMAC_ADAPT_H_
+
+#include "inttypes.h"
+
+struct ETHMAC_ADAPT {
+  uint32_t doit;
+  uint32_t base_tx;
+  uint32_t base_rx;
+  uint32_t length;
+};
+
+typedef volatile struct ETHMAC_ADAPT ethmac_adapt_t;
+
+/* Ethernet MAC adapter init */
+int ethmac_adapt_init(void);
+
+/* User interface */
+int ethmac_adapt_set_base(unsigned int base_rx, unsigned int base_tx);
+int ethmac_adapt_set_length(unsigned int length);
+int ethmac_adapt_go(void);
+
+#endif
+
