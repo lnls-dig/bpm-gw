5e2c1806e4e31576ec3b3423c1d08c30bc631fdb
Lucas Russo <lerwys@gmail.com>
2013-02-17 16:51:03 -0300

    hdl/syn/*: update synth files

--- hdl/syn/dbe_bpm_ebone/Manifest.py
+++ hdl/syn/dbe_bpm_ebone/Manifest.py
@@ -4,7 +4,7 @@ action = "synthesis"
 syn_device = "xc6vlx240t"
 syn_grade = "-1"
 syn_package = "ff1156"
-syn_top = "dbe_bpm_ebone.vhd"
+syn_top = "dbe_bpm_ebone"
 syn_project = "dbe_bpm_ebone.xise"
 
 modules = { "local" : [ "../../top/ml_605/dbe_bpm_ebone" ] };
---
+++ hdl/syn/dbe_bpm_ebone/_impact.cmd
@@ -0,0 +1,36 @@
+setMode -bs
+setMode -ss
+setMode -sm
+setMode -hw140
+setMode -spi
+setMode -acecf
+setMode -acempm
+setMode -pff
+loadProjectFile -file "/home/lerwys/Repos/bpm-sw/hdl/syn/dbe_bpm_ebone/auto_project.ipf"
+setMode -ss
+setMode -sm
+setMode -hw140
+setMode -spi
+setMode -acecf
+setMode -acempm
+setMode -pff
+setMode -bs
+setMode -bs
+setMode -bs
+setMode -bs
+setMode -bs
+setMode -bs
+assignFile -p 2 -file "/home/lerwys/Repos/bpm-sw/hdl/syn/dbe_bpm_ebone/dbe_bpm_ebone.bit"
+Program -p 2 
+setMode -bs
+setMode -bs
+deleteDevice -position 1
+deleteDevice -position 1
+setMode -bs
+setMode -ss
+setMode -sm
+setMode -hw140
+setMode -spi
+setMode -acecf
+setMode -acempm
+setMode -pff
