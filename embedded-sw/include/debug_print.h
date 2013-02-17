c8754465044d151f0cb9aa3688dc80ddd318e1fe
Lucas Russo <lerwys@gmail.com>
2013-02-17 17:12:59 -0300

    emb-sw/*/debug_print.(c|h): add simple debug print function

---
+++ embedded-sw/include/debug_print.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2012 LNLS (www.lnls.br)
+ * Author: Lucas Russo <lucas.russo@lnls.br>
+ *
+ * Released according to the GNU GPL, version 2 or any later version.
+ */
+
+#ifndef _DEBUG_PRINT_
+#define _DEBUG_PRINT_
+
+#include <stdarg.h>
+#include "pp-printf.h"
+
+//void debug_print(const char *fmt, ...);
+void debug_print2(const char *fmt, const char *data, int len);
+
+#ifdef DEBUG_PRINT
+#define dbg_print(fmt, ...) \
+    pp_printf(fmt, ...)
+#define dbg_print2(fmt, data, len) \
+    debug_print2(fmt, data, len)
+#else
+#define dbg_print(const char *fmt, ...)
+#define dbg_print2(fmt, data, len)
+#endif
+
+#endif
