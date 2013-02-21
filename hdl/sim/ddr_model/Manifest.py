0b864ff8bce5aa2a91e5e947c96ef50618713327
Adrian Byszuk <adrian.byszuk@gmail.com>
2013-02-21 09:44:19 +0100

    Add simulation support for DDR enabled firmware

---
+++ hdl/sim/ddr_model/Manifest.py
@@ -0,0 +1,4 @@
+#don't specify parameters file, because it's only included, not compiled
+files = ["ddr3_model.v",
+        "wiredly.v"]
+
--- hdl/testbench/pcie/Manifest.py
+++ hdl/testbench/pcie/Manifest.py
@@ -22,6 +22,8 @@ vlog_opt = "-i ../../sim/pcie -d SIMULATION"
 # ENABLE_GT has to be set until I figure out a way
 # to force ISIM to work in PIPE simulation mode
 vlog_opt += " -d ENABLE_GT"
+# DDR model options
+vlog_opt += " -i ../../sim/ddr_model -d x1Gb -d sg125 -d x8"
 vsim_opt = "-testplusarg TESTNAME=tf64_pcie_axi"
 
 __import_verilog_lib()
@@ -32,5 +34,6 @@ files = ["board.v",
 modules = {"local" : ["../../modules/pcie",
                       "../../ip_cores/pcie/7k325ffg900",
                       "../../sim/pcie",
+                      "../../sim/ddr_model",
                       "../../top/pcie"]}
 
