ec1d242e7f9d94efbe825da062bda27895b31485
Lucas Russo <lerwys@gmail.com>
2013-03-18 20:31:07 -0300

    wb_fmc516/wbgen/*: update register interface

--- hdl/modules/custom_wishbone/wb_fmc516/wbgen/build_wb.sh
+++ hdl/modules/custom_wishbone/wb_fmc516/wbgen/build_wb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wbgen2 -V wb_fmc516_regs.vhd -H record -p wb_fmc516_regs_pkg.vhd -K ../../../../sim/regs/wb_fmc516_regs.vh -s struct -C fmc516_regs.h -D doc/fmc516_regs_wb.html wb_fmc516_regs.wb
+wbgen2 -V wb_fmc516_regs.vhd -H record -p wb_fmc516_regs_pkg.vhd -K ../../../../sim/regs/wb_fmc516_regs.vh -s struct -C fmc516_regs.h -D doc/fmc516_regs_wb.html -f HTML wb_fmc516_regs.wb
