51f6066247c48bce63b30f86fc9342a9e2342931
Lucas Russo <lerwys@gmail.com>
2013-05-27 19:59:29 -0300

    hdl/*/platform/*: add new chipscope icon modules

--- hdl/platform/virtex6/chipscope/Manifest.py
+++ hdl/platform/virtex6/chipscope/Manifest.py
@@ -1,2 +1,3 @@
 files = ["icon_1_port/chipscope_icon_1_port.ngc", "icon_2_port/chipscope_icon_2_port.ngc", 
-	"icon_4_port/chipscope_icon_4_port.ngc", "ila/chipscope_ila.ngc", "ila/chipscope_ila_8192.ngc"]
+	"icon_4_port/chipscope_icon_4_port.ngc", "icon_7_port/chipscope_icon_7_port.ngc", 
+	"icon_8_port/chipscope_icon_8_port.ngc", "ila/chipscope_ila.ngc", "ila/chipscope_ila_8192.ngc"]
