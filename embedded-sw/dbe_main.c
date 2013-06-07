e738ad878c2deb3694c82a3c2ef098d4b3ab33f8
Lucas Russo <lerwys@gmail.com>
2013-06-07 16:59:53 -0300

    emb-sw/dbe_main.c: disable adc ramp test

--- embedded-sw/dbe_main.c
+++ embedded-sw/dbe_main.c
@@ -454,10 +454,10 @@ void fmc516_test()
   //      ISLA216_TESTIO_REG, i);
   //}
 
-  for (i = 0; i < FMC516_NUM_ISLA216; ++i) {
-    fmc516_isla216_test_ramp(i);
-    pp_printf("> FMC516_ISLA216_ADC%d: ramp test enabled!\n", i);
-  }
+  //for (i = 0; i < FMC516_NUM_ISLA216; ++i) {
+  //  fmc516_isla216_test_ramp(i);
+  //  pp_printf("> FMC516_ISLA216_ADC%d: ramp test enabled!\n", i);
+  //}
 
   //for (i = 0; i < FMC516_NUM_ISLA216; ++i) {
   //  fmc516_isla216_test_midscale(i);
