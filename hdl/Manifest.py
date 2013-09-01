5786f6da656e5d2b199ab7acc877108238280245
Lucas Russo <lerwys@gmail.com>
2013-09-01 11:28:41 -0300

    hdl/Manifest.py: comment out rffe_top module

--- hdl/Manifest.py
+++ hdl/Manifest.py
@@ -3,12 +3,12 @@
 modules = { "local": [
                 "modules/custom_wishbone",
                 "modules/custom_common",
-                "modules/rffe_top",
+                #"modules/rffe_top",
                 "modules/ethmac",
                 "modules/fabric",
                 "ip_cores/general-cores",
                 "ip_cores/etherbone-core",
-				"ip_cores/dsp-cores",
+		"ip_cores/dsp-cores",
                 "platform/virtex6/chipscope"]
 #     "git" : [
 #     ]
