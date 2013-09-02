e19c58ccb64c61b74db6805af79691c5c0bfd48e
Lucas Russo <lerwys@gmail.com>
2013-09-02 15:10:13 -0300

    hdl/modules/*: delete modules that were not suppose to be here
    The modules wb_position_calc and wb_un_cross now
    resides in dsp-cores repository (added as a git submodule)

--- hdl/modules/dbe_wishbone/wb_un_cross/wbgen/build_wb.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-wbgen2 -V wb_bpm_swap_regs.vhd -H record -p xbpm_swap_regs_pkg.vhd -K ../../../../sim/regs/xbpm_swap_regs.vh -s struct -C wb_bpm_swap.h -f html -D doc/xbpm_swap_regs_wb.html xbpm_swap.wb
