1993a12344da8e59f779877b61193dd0df6902e8
Lucas Russo <lerwys@gmail.com>
2013-09-26 15:53:29 -0300

    hdl/top/*/dbe_bpm_dsp_fmc516/*: add position_calc_core ucf file
    This top design will probably fail now, when we update the
    dsp-cores interface... bad!

--- hdl/top/ml_605/dbe_bpm_dsp_fmc516/Manifest.py
+++ hdl/top/ml_605/dbe_bpm_dsp_fmc516/Manifest.py
@@ -1,3 +1,3 @@
-files = [ "dbe_bpm_dsp.vhd", "sys_pll.vhd", "clk_gen.vhd", "dbe_bpm_dsp.ucf" ];
+files = [ "dbe_bpm_dsp.vhd", "sys_pll.vhd", "clk_gen.vhd", "dbe_bpm_dsp.ucf", "position_calc_core.ucf" ];
 
 modules = { "local" : ["../../.." ] };
