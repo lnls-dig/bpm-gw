ac46748539cef956ffce448c20e65c5b8c014cdc
Lucas Russo <lerwys@gmail.com>
2013-09-26 15:46:31 -0300

    hdl/platform/virtex6/*: add chipscope and dds ip cores

---
+++ hdl/platform/virtex6/chipscope/icon_13_port/chipscope_icon_13_port.vhd
@@ -0,0 +1,41 @@
+-------------------------------------------------------------------------------
+-- Copyright (c) 2013 Xilinx, Inc.
+-- All Rights Reserved
+-------------------------------------------------------------------------------
+--   ____  ____
+--  /   /\/   /
+-- /___/  \  /    Vendor     : Xilinx
+-- \   \   \/     Version    : 13.4
+--  \   \         Application: XILINX CORE Generator
+--  /   /         Filename   : chipscope_icon_13_port.vhd
+-- /___/   /\     Timestamp  : Tue Jul 23 14:56:51 BRT 2013
+-- \   \  /  \
+--  \___\/\___\
+--
+-- Design Name: VHDL Synthesis Wrapper
+-------------------------------------------------------------------------------
+-- This wrapper is used to integrate with Project Navigator and PlanAhead
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ENTITY chipscope_icon_13_port IS
+  port (
+    CONTROL0: inout std_logic_vector(35 downto 0);
+    CONTROL1: inout std_logic_vector(35 downto 0);
+    CONTROL2: inout std_logic_vector(35 downto 0);
+    CONTROL3: inout std_logic_vector(35 downto 0);
+    CONTROL4: inout std_logic_vector(35 downto 0);
+    CONTROL5: inout std_logic_vector(35 downto 0);
+    CONTROL6: inout std_logic_vector(35 downto 0);
+    CONTROL7: inout std_logic_vector(35 downto 0);
+    CONTROL8: inout std_logic_vector(35 downto 0);
+    CONTROL9: inout std_logic_vector(35 downto 0);
+    CONTROL10: inout std_logic_vector(35 downto 0);
+    CONTROL11: inout std_logic_vector(35 downto 0);
+    CONTROL12: inout std_logic_vector(35 downto 0));
+END chipscope_icon_13_port;
+
+ARCHITECTURE chipscope_icon_13_port_a OF chipscope_icon_13_port IS
+BEGIN
+
+END chipscope_icon_13_port_a;
---
+++ hdl/platform/virtex6/ip_cores/Manifest.py
@@ -0,0 +1,3 @@
+files = ["dds_adc_input.ngc",
+	"dds_adc_input.v"
+	]
