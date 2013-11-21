fba167bfeca1589bfa1558cc0e12923b42727382
Lucas Russo <lerwys@gmail.com>
2013-11-21 10:03:14 -0200

    hdl/modules/pcie/Manifest.py: update Manifest file

--- hdl/modules/pcie/Manifest.py
+++ hdl/modules/pcie/Manifest.py
@@ -2,5 +2,6 @@ modules = {"local" : ["common"] }
 
 files = ["bpm_pcie_a7.vhd",
         "bpm_pcie_k7.vhd",
-        "bpm_pcie_ml605.vhd"]
+        "bpm_pcie_ml605.vhd",
+        "bpm_pcie_pkg.vhd"]
 
