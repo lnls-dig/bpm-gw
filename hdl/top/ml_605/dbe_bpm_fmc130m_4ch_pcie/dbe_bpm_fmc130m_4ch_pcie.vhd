fe9339e88f89fc7658afbcdc09ab860d526523a7
Lucas Russo <lerwys@gmail.com>
2013-11-21 10:03:52 -0200

    hdl/top/ml_605/*_pcie/*: update top PCIe files for ML605
    - Removed declaration of bpm_pcie_* component as
        we use the bpm_pcie_pkg package now.
    - Changed DDR calibration to FULL instead of FAST.
    this is recommended by Xilinx.

--- hdl/top/ml_605/dbe_bpm_fmc130m_4ch_pcie/dbe_bpm_fmc130m_4ch_pcie.vhd
+++ hdl/top/ml_605/dbe_bpm_fmc130m_4ch_pcie/dbe_bpm_fmc130m_4ch_pcie.vhd
@@ -40,6 +40,8 @@ use work.etherbone_pkg.all;
 use work.fmc_adc_pkg.all;
 -- Data Acquisition core
 use work.acq_core_pkg.all;
+-- PCIe Core
+use work.bpm_pcie_pkg.all;
 
 library UNISIM;
 use UNISIM.vcomponents.all;
@@ -660,106 +662,6 @@ architecture rtl of dbe_bpm_fmc130m_4ch_pcie is
   );
   end component;
 
-  -- PCIe Core
-  component bpm_pcie_ml605
-  generic (
-    RST_ACT_LOW  : integer := 1;
-    SIMULATION   : string := "FALSE";
-    INSTANTIATED : string := "FALSE";
-    -- ****
-    -- PCIe core parameters
-    -- ****
-    constant pcieLanes : integer := 4;
-    PL_FAST_TRAIN      : string  := "FALSE";
-    PIPE_SIM_MODE      : string  := "FALSE";
-    --***************************************************************************
-    -- Necessary parameters for DDR core support
-    -- (dependent on memory chip connected to FPGA, not to be modified at will)
-    --***************************************************************************
-    constant DDR_DQ_WIDTH      : integer := 64;
-    constant DDR_PAYLOAD_WIDTH : integer := 256;
-    constant DDR_DQS_WIDTH     : integer := 8;
-    constant DDR_DM_WIDTH      : integer := 8;
-    constant DDR_ROW_WIDTH     : integer := 14;
-    constant DDR_BANK_WIDTH    : integer := 3;
-    constant DDR_CK_WIDTH      : integer := 1;
-    constant DDR_CKE_WIDTH     : integer := 1;
-    constant DDR_ODT_WIDTH     : integer := 1;
-    SIM_BYPASS_INIT_CAL        : string  := "FAST"
-                                     -- # = "OFF" -  Complete memory init &
-                                     --              calibration sequence
-                                     -- # = "SKIP" - Not supported
-                                     -- # = "FAST" - Complete memory init & use
-                                     --              abbreviated calib sequence
-    );
-  port (
-    --DDR3 memory pins
-    ddr3_dq                                 : inout std_logic_vector(DDR_DQ_WIDTH-1 downto 0);
-    ddr3_dqs_p                              : inout std_logic_vector(DDR_DQS_WIDTH-1 downto 0);
-    ddr3_dqs_n                              : inout std_logic_vector(DDR_DQS_WIDTH-1 downto 0);
-    ddr3_addr                               : out   std_logic_vector(DDR_ROW_WIDTH-1 downto 0);
-    ddr3_ba                                 : out   std_logic_vector(DDR_BANK_WIDTH-1 downto 0);
-    ddr3_cs_n                               : out   std_logic_vector(0 downto 0);
-    ddr3_ras_n                              : out   std_logic;
-    ddr3_cas_n                              : out   std_logic;
-    ddr3_we_n                               : out   std_logic;
-    ddr3_reset_n                            : out   std_logic;
-    ddr3_ck_p                               : out   std_logic_vector(DDR_CK_WIDTH-1 downto 0);
-    ddr3_ck_n                               : out   std_logic_vector(DDR_CK_WIDTH-1 downto 0);
-    ddr3_cke                                : out   std_logic_vector(DDR_CKE_WIDTH-1 downto 0);
-    ddr3_dm                                 : out   std_logic_vector(DDR_DM_WIDTH-1 downto 0);
-    ddr3_odt                                : out   std_logic_vector(DDR_ODT_WIDTH-1 downto 0);
-    -- PCIe transceivers
-    pci_exp_rxp                             : in  std_logic_vector(pcieLanes - 1 downto 0);
-    pci_exp_rxn                             : in  std_logic_vector(pcieLanes - 1 downto 0);
-    pci_exp_txp                             : out std_logic_vector(pcieLanes - 1 downto 0);
-    pci_exp_txn                             : out std_logic_vector(pcieLanes - 1 downto 0);
-    -- Necessity signals
-    ddr_sys_clk_p                           : in std_logic; --200 MHz DDR core clock (connect through BUFG or PLL)
-    ddr_sys_clk_n                           : in std_logic; --200 MHz DDR core clock (connect through BUFG or PLL)
-    sys_clk_p                               : in std_logic; --100 MHz PCIe Clock (connect directly to input pin)
-    sys_clk_n                               : in std_logic; --100 MHz PCIe Clock
-    sys_rst_n                               : in std_logic; --Reset to PCIe core
-
-    -- DDR memory controller interface --
-    -- uncomment when instantiating in another project
-    ddr_core_rst                            : in  std_logic;
-    memc_ui_clk                             : out std_logic;
-    memc_ui_rst                             : out std_logic;
-    memc_cmd_rdy                            : out std_logic;
-    memc_cmd_en                             : in  std_logic;
-    memc_cmd_instr                          : in  std_logic_vector(2 downto 0);
-    memc_cmd_addr                           : in  std_logic_vector(31 downto 0);
-    memc_wr_en                              : in  std_logic;
-    memc_wr_end                             : in  std_logic;
-    memc_wr_mask                            : in  std_logic_vector(DDR_PAYLOAD_WIDTH/8-1 downto 0);
-    memc_wr_data                            : in  std_logic_vector(DDR_PAYLOAD_WIDTH-1 downto 0);
-    memc_wr_rdy                             : out std_logic;
-    memc_rd_data                            : out std_logic_vector(DDR_PAYLOAD_WIDTH-1 downto 0);
-    memc_rd_valid                           : out std_logic;
-    -- memory arbiter interface
-    memarb_acc_req                          : in  std_logic;
-    memarb_acc_gnt                          : out std_logic;
-    --/ DDR memory controller interface
-
-    -- Wishbone interface --
-    -- uncomment when instantiating in another project
-    clk_i                                   : in  std_logic;
-    rst_i                                   : in  std_logic;
-    ack_i                                   : in  std_logic;
-    dat_i                                   : in  std_logic_vector(63 downto 0);
-    addr_o                                  : out std_logic_vector(28 downto 0);
-    dat_o                                   : out std_logic_vector(63 downto 0);
-    we_o                                    : out std_logic;
-    stb_o                                   : out std_logic;
-    sel_o                                   : out std_logic;
-    cyc_o                                   : out std_logic;
-    --/ Wishbone interface
-    -- Additional exported signals for instantiation
-    ext_rst_o                               : out std_logic
-  );
-  end component;
-
   -- Functions
   -- Generate dummy (0) values
   function f_zeros(size : integer)
@@ -890,9 +792,8 @@ begin
   cmp_bpm_pcie_ml605 : bpm_pcie_ml605
   generic map (
     RST_ACT_LOW                             => 1,
-    INSTANTIATED                            => "TRUE",
-    --SIM_BYPASS_INIT_CAL                     => "OFF" -- Full calibration sequence
-    SIM_BYPASS_INIT_CAL                     => "FAST" -- Fast calibration sequence
+    SIM_BYPASS_INIT_CAL                     => "OFF" -- Full calibration sequence
+    --SIM_BYPASS_INIT_CAL                     => "FAST" -- Fast calibration sequence
   )
   port map (
     --DDR3 memory pins
@@ -918,7 +819,6 @@ begin
     pci_exp_txn                             => pci_exp_txn_o,
     -- Necessity signals
     ddr_sys_clk_p                           => clk_200mhz,   --200 MHz DDR core clock (connect through BUFG or PLL)
-    ddr_sys_clk_n                           => '0',          --200 MHz DDR core clock (connect through BUFG or PLL)
     sys_clk_p                               => pcie_clk_p_i,  --100 MHz PCIe Clock (connect directly to input pin)
     sys_clk_n                               => pcie_clk_n_i,  --100 MHz PCIe Clock
     sys_rst_n                               => pcie_rst_n_i, -- PCIe core reset
