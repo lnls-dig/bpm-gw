b4f999a76a0939a01f9245f426bb085e7b46cf9b
Lucas Russo <lerwys@gmail.com>
2013-12-04 09:28:47 -0200

    hdl/top/*/dbe_bpm_fmc130m_4ch_pcie/*: add .xcf constraints file
    Also, updated Manifest file

--- hdl/top/ml_605/dbe_bpm_fmc130m_4ch_pcie/Manifest.py
+++ hdl/top/ml_605/dbe_bpm_fmc130m_4ch_pcie/Manifest.py
@@ -1,5 +1,6 @@
 #files = [ "dbe_bpm_fmc130m_4ch_pcie.vhd", "sys_pll.vhd", "clk_gen.vhd", "dbe_bpm_fmc130m_4ch_pcie.ucf", "pcie.ucf" ];
-files = [ "dbe_bpm_fmc130m_4ch_pcie.vhd", "sys_pll.vhd", "clk_gen.vhd", "dbe_bpm_fmc130m_4ch_pcie.ucf" ];
+files = [ "dbe_bpm_fmc130m_4ch_pcie.vhd", "sys_pll.vhd", "clk_gen.vhd", "dbe_bpm_fmc130m_4ch_pcie.ucf",
+"dbe_bpm_fmc130m_4ch_pcie.xcf" ];
 
 modules = { "local" :
             ["../../..",
