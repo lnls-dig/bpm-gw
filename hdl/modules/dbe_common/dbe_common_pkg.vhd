c3bf8469090ee1783d73c1f3a803c0e91fffad13
Lucas Russo <lerwys@gmail.com>
2014-01-26 12:37:14 -0200

    hdl/modules/dbe_common/*: add generic pipeline stages in reset_synch

--- hdl/modules/dbe_common/dbe_common_pkg.vhd
+++ hdl/modules/dbe_common/dbe_common_pkg.vhd
@@ -3,17 +3,22 @@ use ieee.std_logic_1164.all;
 
 package dbe_common_pkg is
 
-    --------------------------------------------------------------------
-    -- Components
-    --------------------------------------------------------------------
-
-    component reset_synch
-    port
-    (
-        clk_i                     : in  std_logic;
-        arst_n_i                : in  std_logic;
-        rst_n_o                  : out std_logic
-    );
-    end component;
+  --------------------------------------------------------------------
+  -- Components
+  --------------------------------------------------------------------
+  component reset_synch
+  generic
+  (
+    -- Select 1 for no pipeline, and greater than 1 to insert
+    -- pipeline stages
+    g_pipeline                             : natural := 4
+  );
+  port
+  (
+    clk_i                                  : in  std_logic;
+    arst_n_i                               : in  std_logic;
+    rst_n_o                                : out std_logic
+  );
+  end component;
 
 end dbe_common_pkg;
