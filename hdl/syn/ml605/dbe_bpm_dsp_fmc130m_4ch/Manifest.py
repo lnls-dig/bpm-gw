135ce5e83d51c2b4d403095f66c4432cf2a85534
Lucas Russo <lerwys@gmail.com>
2014-04-28 14:33:08 -0300

    hdl/syn/ml605/*dsp_fmc130m_4ch/*: change to new hdlmake semantics
    Added syn_tool variable to Manifest file and
    a remote synthesis script based on the example
    from hdlmake repository

--- hdl/syn/ml605/dbe_bpm_dsp_fmc130m_4ch/Manifest.py
+++ hdl/syn/ml605/dbe_bpm_dsp_fmc130m_4ch/Manifest.py
@@ -6,5 +6,6 @@ syn_grade = "-1"
 syn_package = "ff1156"
 syn_top = "dbe_bpm_dsp"
 syn_project = "dbe_bpm_dsp.xise"
+syn_tool = "ise"
 
 modules = { "local" : [ "../../../top/ml_605/dbe_bpm_dsp_fmc130m_4ch" ] };
