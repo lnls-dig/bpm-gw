7b41151f8bed5c82ed1b2baedcef168938e929c9
Lucas Russo <lerwys@gmail.com>
2014-05-10 00:19:24 -0300

    hdl/*/*/fmc_adc_buf.vhd: fix wrong IOSTANDARD
    For single-ended inputs, it is necessary to use
    A single-ended standard, obviously

--- hdl/modules/fmc_adc_common/fmc_adc_buf.vhd
+++ hdl/modules/fmc_adc_common/fmc_adc_buf.vhd
@@ -263,7 +263,7 @@ begin
 
       cmp_ibuf_adc_data_ch0 : ibuf
       generic map(
-        IOSTANDARD                            => "LVDS_25"
+        IOSTANDARD                            => "LVCMOS25"
       )
       port map(
         i                                     => adc_data_ch0_p_t(i),
@@ -272,7 +272,7 @@ begin
 
       cmp_ibuf_adc_data_ch1 : ibuf
       generic map(
-        IOSTANDARD                            => "LVDS_25"
+        IOSTANDARD                            => "LVCMOS25"
       )
       port map(
         i                                     => adc_data_ch1_p_t(i),
@@ -281,7 +281,7 @@ begin
 
       cmp_ibuf_adc_data_ch2 : ibuf
       generic map(
-        IOSTANDARD                            => "LVDS_25"
+        IOSTANDARD                            => "LVCMOS25"
       )
       port map(
         i                                     => adc_data_ch2_p_t(i),
@@ -290,7 +290,7 @@ begin
 
       cmp_ibuf_adc_data_ch3 : ibuf
       generic map(
-        IOSTANDARD                            => "LVDS_25"
+        IOSTANDARD                            => "LVCMOS25"
       )
       port map(
         i                                     => adc_data_ch3_p_t(i),
