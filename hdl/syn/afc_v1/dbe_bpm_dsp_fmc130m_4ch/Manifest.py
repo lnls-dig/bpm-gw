e60bf00595951a7673f28c722c6eba4ab5d4fd40
Lucas Russo <lerwys@gmail.com>
2014-06-18 18:02:12 -0300

    hdl/syn/afc_v1/*/Manifest.py: add ise_tool parameter
    In the new version of hdlmake, we need to supply
    the ISE version we want to use

--- hdl/syn/afc_v1/dbe_bpm_dsp_fmc130m_4ch/Manifest.py
+++ hdl/syn/afc_v1/dbe_bpm_dsp_fmc130m_4ch/Manifest.py
@@ -7,5 +7,6 @@ syn_package = "ffg1156"
 syn_top = "dbe_bpm_dsp"
 syn_project = "dbe_bpm_dsp.xise"
 syn_tool = "ise"
+syn_ise_version = "14.6"
 
 modules = { "local" : [ "../../../top/afc_v1/dbe_bpm_dsp_fmc130m_4ch" ] };
