f3d4ffd59fd9c9dbaaaa11a72749d88e30caf7d7
Lucas Russo <lerwys@gmail.com>
2014-09-14 11:50:37 -0300

    hdl/modules/*/wb_acq_core/acq_sel_chan.vhd: fix bad indentation

--- hdl/modules/dbe_wishbone/wb_acq_core/acq_sel_chan.vhd
+++ hdl/modules/dbe_wishbone/wb_acq_core/acq_sel_chan.vhd
@@ -33,19 +33,19 @@ port
 (
   clk_i                                     : in  std_logic;
   rst_n_i                                   : in  std_logic;
-  
-  -----------------------------
-  -- Acquisiton Interface
-  -----------------------------
+
+-----------------------------
+-- Acquisiton Interface
+-----------------------------
   acq_val_low_i                             : in t_acq_val_half_array(g_acq_num_channels-1 downto 0);
   acq_val_high_i                            : in t_acq_val_half_array(g_acq_num_channels-1 downto 0);
   acq_dvalid_i                              : in std_logic_vector(g_acq_num_channels-1 downto 0);
   acq_trig_i                                : in std_logic_vector(g_acq_num_channels-1 downto 0);
   acq_curr_chan_id_i                        : in unsigned(c_chan_id_width-1 downto 0);
-  
-  -----------------------------
-  -- Output Interface. 
-  -----------------------------
+
+-----------------------------
+-- Output Interface.
+-----------------------------
   acq_data_o                                : out std_logic_vector(c_acq_chan_max_w-1 downto 0);
   acq_dvalid_o                              : out std_logic;
   acq_trig_o                                : out std_logic
@@ -53,40 +53,40 @@ port
 end acq_sel_chan;
 
 architecture rtl of acq_sel_chan is
-  
+
   signal acq_data_marsh_demux               : std_logic_vector(c_acq_chan_max_w-1 downto 0);
   signal acq_trig_demux                     : std_logic;
   signal acq_dvalid_demux                   : std_logic;
-  
+
   signal acq_data_marsh_demux_reg           : std_logic_vector(c_acq_chan_max_w-1 downto 0);
   signal acq_trig_demux_reg                 : std_logic;
   signal acq_dvalid_demux_reg               : std_logic;
 
 begin
-  
-  acq_data_marsh_demux                   <=
+
+ acq_data_marsh_demux                   <=
     f_acq_chan_conv_val(f_acq_chan_marshall_val(acq_val_high_i(to_integer(acq_curr_chan_id_i)),
-      acq_val_low_i(to_integer(acq_curr_chan_id_i))));
-  acq_trig_demux                         <= acq_trig_i(to_integer(acq_curr_chan_id_i));
-  acq_dvalid_demux                       <= acq_dvalid_i(to_integer(acq_curr_chan_id_i));
+                                                acq_val_low_i(to_integer(acq_curr_chan_id_i))));
+ acq_trig_demux                         <= acq_trig_i(to_integer(acq_curr_chan_id_i));
+ acq_dvalid_demux                       <= acq_dvalid_i(to_integer(acq_curr_chan_id_i));
 
  p_reg_demux : process (clk_i)
-  begin
-    if rising_edge(clk_i) then
-      if rst_n_i = '0' then
-        acq_data_marsh_demux_reg <= (others => '0');
-	acq_dvalid_demux_reg <= '0';
-	acq_trig_demux_reg <= '0';
-      else
-        acq_data_marsh_demux_reg <= acq_data_marsh_demux;
-	acq_dvalid_demux_reg <= acq_dvalid_demux;
-	acq_trig_demux_reg <= acq_trig_demux;
-      end if;
-    end if;
-  end process;
-  
-  acq_data_o                               <= acq_data_marsh_demux_reg; 
-  acq_dvalid_o                             <= acq_dvalid_demux_reg;
-  acq_trig_o                               <= acq_trig_demux_reg; 
+ begin
+   if rising_edge(clk_i) then
+     if rst_n_i = '0' then
+       acq_data_marsh_demux_reg <= (others => '0');
+       acq_dvalid_demux_reg <= '0';
+       acq_trig_demux_reg <= '0';
+     else
+       acq_data_marsh_demux_reg <= acq_data_marsh_demux;
+       acq_dvalid_demux_reg <= acq_dvalid_demux;
+       acq_trig_demux_reg <= acq_trig_demux;
+     end if;
+   end if;
+ end process;
+
+ acq_data_o                               <= acq_data_marsh_demux_reg;
+ acq_dvalid_o                             <= acq_dvalid_demux_reg;
+ acq_trig_o                               <= acq_trig_demux_reg;
 
 end rtl;
