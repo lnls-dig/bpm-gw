b8c3f009115c6ec3bf656a36794723d740b708d6
Lucas Russo <lerwys@gmail.com>
2015-02-25 20:22:52 -0300

    hdl/platform/Manifest.py: update to include artix7 chipscope

--- hdl/platform/Manifest.py
+++ hdl/platform/Manifest.py
@@ -4,7 +4,7 @@ def __dirs():
 	if (target == "xilinx" and syn_device[0:4].upper()=="XC6V"):
 		dirs.extend(["virtex6/chipscope", "virtex6/ip_cores"]);
 	elif (target == "xilinx" and syn_device[0:4].upper()=="XC7A"):
-		dirs.extend(["artix7/ip_cores"]);
+		dirs.extend(["artix7/chipscope", "artix7/ip_cores"]);
 	#else: #add paltform here and generate the corresponding ip cores
 	return dirs
 
