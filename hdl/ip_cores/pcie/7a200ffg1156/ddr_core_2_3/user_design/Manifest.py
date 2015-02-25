ef58242aa3badbe6b45439e93142b6afd9d3dbec
Lucas Russo <lerwys@gmail.com>
2015-02-25 20:27:42 -0300

    hdl/ip_cores/pcie/*/ddr_core_2_3/*: add Xilinx DDR3 MIG 2.3 core

---
+++ hdl/ip_cores/pcie/7a200ffg1156/ddr_core_2_3/user_design/Manifest.py
@@ -0,0 +1,10 @@
+files = ["rtl/ddr_core.vhd",
+         "rtl/ddr_core_mig.vhd",
+         "rtl/clocking/",
+         "rtl/controller/",
+         "rtl/ecc/",
+         "rtl/ip_top/",
+         "rtl/phy/",
+         "rtl/ui/",
+         "constraints/ddr_core.xdc"]
+
