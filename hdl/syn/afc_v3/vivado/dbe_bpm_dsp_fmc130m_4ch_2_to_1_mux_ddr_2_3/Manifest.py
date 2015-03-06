153a9ee93611ca8d41215accf5c679e9b2812c01
Lucas Russo <lerwys@gmail.com>
2015-03-06 18:20:25 -0300

    hdl/syn/afc_v3/vivado/*: add SDB meta template project.vhd
    The project.vhd file should be autogenerated by hdlmake,
    inj order to keep consistency in the git commits, date, and so on.
    It is possible to changes the parameters by hand, but this is
    very tedious and error prone.

--- hdl/syn/afc_v3/vivado/dbe_bpm_dsp_fmc130m_4ch_2_to_1_mux_ddr_2_3/Manifest.py
+++ hdl/syn/afc_v3/vivado/dbe_bpm_dsp_fmc130m_4ch_2_to_1_mux_ddr_2_3/Manifest.py
@@ -8,5 +8,7 @@ syn_top = "dbe_bpm_dsp"
 syn_project = "dbe_bpm_dsp"
 syn_tool = "vivado"
 
+files = ["project.vhd"];
+
 modules = { "local" : [ "../../../../top/afc_v3/vivado/dbe_bpm_dsp_fmc130m_4ch_2_to_1_mux_ddr_2_3" ] };
 
