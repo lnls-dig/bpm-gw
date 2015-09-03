78540d514c2cf4ce00d7b8ed5a5e40942a1640da
Lucas Russo <lerwys@gmail.com>
2015-09-03 17:03:02 -0300

    wb_acq_core/*: retag data_id DPRAM samples to PRE_TRIG
    We use the data ID tag to count the valid samples
    written do DDR. As the DPRAM only outputs valid
    samples, we will almost certainly have WAIT_TRIG
    and POST_TRIG samples only, and not PRE_TRIG samples.
    This means that downstream modules will not count these
    samples as valid.
    
    The fix was to retag the samples from WAIT_TRIG to
    PRE_TRIG.

--- hdl/modules/dbe_wishbone/wb_acq_core/acq_multishot_dpram.vhd
+++ hdl/modules/dbe_wishbone/wb_acq_core/acq_multishot_dpram.vhd
@@ -71,14 +71,22 @@ architecture rtl of acq_multishot_dpram is
   constant c_dpram_depth                    : integer := f_log2_size(g_multishot_ram_size);
   constant c_dpram_width                    : integer := g_header_out_width+g_data_width;
 
+  constant c_dpram_header_top_idx           : natural := g_header_out_width+g_data_width-1;
+  constant c_dpram_header_bot_idx           : natural := g_data_width;
+  constant c_dpram_payload_top_idx          : natural := g_data_width-1;
+  constant c_dpram_payload_bot_idx          : natural := 0;
+
   signal dpram_trig                         : std_logic;
 
   signal dpram_addra_cnt                    : unsigned(c_dpram_depth-1 downto 0);
   signal dpram_addra_trig                   : unsigned(c_dpram_depth-1 downto 0);
   signal dpram_addra_post_done              : unsigned(c_dpram_depth-1 downto 0);
   signal dpram_addrb_cnt                    : unsigned(c_dpram_depth-1 downto 0);
-  signal dpram_valid                        : std_logic;
   signal dpram_valid_t                      : std_logic;
+  signal dpram_valid_t1                     : std_logic;
+  signal dpram_trigger                      : std_logic;
+  signal dpram_data_id                      : std_logic_vector(2 downto 0);
+  signal dpram_data                         : std_logic_vector(g_data_width-1 downto 0);
 
   signal dpram0_dina                        : std_logic_vector(c_dpram_width-1 downto 0);
   signal dpram0_addra                       : std_logic_vector(c_dpram_depth-1 downto 0);
@@ -91,6 +99,11 @@ architecture rtl of acq_multishot_dpram is
   signal dpram1_wea                         : std_logic;
   signal dpram1_addrb                       : std_logic_vector(c_dpram_depth-1 downto 0);
   signal dpram1_doutb                       : std_logic_vector(c_dpram_width-1 downto 0);
+
+  signal dpram_dout                         : std_logic_vector(g_header_out_width+g_data_width-1 downto 0);
+  signal dpram_valid                        : std_logic;
+  signal dpram_dout_retag                   : std_logic_vector(g_header_out_width+g_data_width-1 downto 0);
+  signal dpram_valid_retag                  : std_logic;
 
 begin
 
@@ -194,7 +207,7 @@ begin
       if fs_rst_n_i = '0' then
         dpram_addrb_cnt <= (others => '0');
         dpram_valid_t   <= '0';
-        dpram_valid     <= '0';
+        dpram_valid_t1  <= '0';
       else
         if acq_post_trig_done_i = '1' then
           dpram_addrb_cnt <= dpram_addra_trig - pre_trig_samples_i(c_dpram_depth-1 downto 0);
@@ -206,17 +219,54 @@ begin
         end if;
 
         -- Account for DPRAM 1 cycle latency
-        dpram_valid <= dpram_valid_t;
+        dpram_valid_t1 <= dpram_valid_t;
 
       end if;
     end if;
-  end process p_dpram_addrb_cnt;
+  end process;
 
   dpram0_addrb <= std_logic_vector(dpram_addrb_cnt);
   dpram1_addrb <= std_logic_vector(dpram_addrb_cnt);
 
   -- DPRAM output mux. When writing to DPRAM 0, reads from DPRAM 1 and vice-versa
-  dpram_dout_o   <= dpram0_doutb when buffer_sel_i = '1' else dpram1_doutb;
-  dpram_valid_o  <= dpram_valid;
+  dpram_dout   <= dpram0_doutb when buffer_sel_i = '1' else dpram1_doutb;
+  dpram_valid  <= dpram_valid_t1;
+
+  -- Extract trigger from dpram data
+  dpram_trigger <= dpram_dout(c_acq_header_trigger_idx+c_dpram_header_bot_idx);
+
+  -- Extract data_id from dpram data
+  dpram_data_id <= dpram_dout(c_acq_header_id_top_idx+c_dpram_header_bot_idx downto
+                          c_acq_header_id_bot_idx+c_dpram_header_bot_idx);
+
+  dpram_data <= dpram_dout(c_dpram_payload_top_idx downto c_dpram_payload_bot_idx);
+
+  -- Change DPRAM tag from wait_trig to pre_samples, as we only write to DPRAM
+  -- exactly the samples to be written to DDR and we can have samples tagged with
+  -- wait_trig (which are not gonna be counter for on next modules)
+  p_dpram_retag : process (fs_clk_i)
+  begin
+    if rising_edge(fs_clk_i) then
+      if fs_rst_n_i = '0' then
+        dpram_dout_retag <= (others => '0');
+        dpram_valid_retag <= '0';
+      else
+        dpram_valid_retag <= dpram_valid;
+
+        if dpram_valid = '1' then
+          -- Retag data with Pre Trigger state
+          if dpram_data_id = "011" then -- Wait Trigger State
+            dpram_dout_retag <= "010" & dpram_trigger & dpram_data;
+          else
+            dpram_dout_retag <= dpram_dout;
+          end if;
+        end if;
+
+      end if;
+    end if;
+  end process;
+
+  dpram_dout_o   <= dpram_dout_retag;
+  dpram_valid_o  <= dpram_valid_retag;
 
 end rtl;
