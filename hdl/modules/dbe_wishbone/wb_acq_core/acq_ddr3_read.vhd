85d36a0d94600223cf0dbf2fc70f96f97554efd9
Lucas Russo <lerwys@gmail.com>
2015-09-03 17:05:50 -0300

    wb_acq_core/*: fix DDR3 reading address for multishot
    We were not calculating the samples in a multishot
    acquisition correctly, causing the module to read
    samples from a wrong address.

--- hdl/modules/dbe_wishbone/wb_acq_core/acq_ddr3_read.vhd
+++ hdl/modules/dbe_wishbone/wb_acq_core/acq_ddr3_read.vhd
@@ -156,8 +156,11 @@ architecture rtl of acq_ddr3_read is
   signal lmt_chan_curr_width                : unsigned(c_acq_chan_max_w_log2-1 downto 0);
   signal rb_ddr_trig_addr                   : unsigned(g_ddr_addr_width-1 downto 0);
   -- For intermediate multiplication result
+  signal lmt_full_pkt_addr_ss               : unsigned(63 downto 0);
+  signal lmt_full_pkt_addr_ms               : unsigned(63 downto 0);
   signal lmt_pre_pkt_addr                   : unsigned(63 downto 0);
   signal lmt_pre_full_addr                  : unsigned(63 downto 0);
+
 
   -- DDR3 Signals
   signal ddr_data_in                        : std_logic_vector(c_ddr_payload_width-1 downto 0);
@@ -303,8 +306,11 @@ begin
     end if;
   end process;
 
+  lmt_full_pkt_addr_ss <= lmt_full_pkt_size*c_ddr_fc_payload_ratio_log2(to_integer(lmt_curr_chan_id_i));
+  lmt_full_pkt_addr_ms <= lmt_full_pkt_addr_ss(lmt_full_pkt_addr_ss'left-lmt_shots_nb'length downto 0)*(lmt_shots_nb-1);
   lmt_pre_pkt_addr <= lmt_pre_pkt_size*c_ddr_fc_payload_ratio_log2(to_integer(lmt_curr_chan_id_i));
-  lmt_pre_full_addr <= lmt_pre_pkt_addr(lmt_pre_pkt_addr'left-lmt_shots_nb'length downto 0)*lmt_shots_nb;
+
+  lmt_pre_full_addr <= lmt_full_pkt_addr_ms + lmt_pre_pkt_addr;
 
   ----------------------------------------------------------------------------
   -- Start reading
