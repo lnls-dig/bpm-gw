6e8670439a1407358d5c81cea9cd7c52a9f0e6f0
Lucas Russo <lerwys@gmail.com>
2015-10-30 15:32:20 -0200

    syn/afc_v3/*: rename Vivado design files to "dbe_bpm"

--- hdl/syn/afc_v3/vivado/dbe_bpm/Manifest.py
+++ hdl/syn/afc_v3/vivado/dbe_bpm/Manifest.py
@@ -4,13 +4,13 @@ action = "synthesis"
 syn_device = "xc7a200t"
 syn_grade = "-1"
 syn_package = "ffg1156"
-syn_top = "dbe_bpm_dsp"
-syn_project = "dbe_bpm_dsp"
+syn_top = "dbe_bpm"
+syn_project = "dbe_bpm"
 syn_tool = "vivado"
 
 files = ["project.vhd"];
 
 machine_pkg = "uvx_130M"
 
-modules = { "local" : [ "../../../../top/afc_v3/vivado/dbe_bpm_dsp_fmc130m_4ch_2_to_1_mux_ddr_2_3" ] };
+modules = { "local" : [ "../../../../top/afc_v3/vivado/dbe_bpm" ] };
 
