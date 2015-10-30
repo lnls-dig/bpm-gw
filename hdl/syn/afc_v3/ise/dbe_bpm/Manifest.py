8caeb7cf078026c71b5ce23d1a791c08970f0c31
Lucas Russo <lerwys@gmail.com>
2015-10-30 15:15:12 -0200

    syn/afc_v3/*: rename ISE design files to ise/dbe_bpm

--- hdl/syn/afc_v3/ise/dbe_bpm/Manifest.py
+++ hdl/syn/afc_v3/ise/dbe_bpm/Manifest.py
@@ -4,11 +4,11 @@ action = "synthesis"
 syn_device = "xc7a200t"
 syn_grade = "-1"
 syn_package = "ffg1156"
-syn_top = "dbe_bpm_dsp"
-syn_project = "dbe_bpm_dsp.xise"
+syn_top = "dbe_bpm"
+syn_project = "dbe_bpm.xise"
 syn_tool = "ise"
 
 machine_pkg = "uvx_130M"
 
-modules = { "local" : [ "../../../top/afc_v3/dbe_bpm_dsp_fmc130m_4ch_2_to_1_mux" ] };
+modules = { "local" : [ "../../../top/afc_v3/dbe_bpm" ] };
 
