a7fded3c49389c7d131399a052eb74fa0dfd7383
Lucas Russo <lerwys@gmail.com>
2015-11-24 18:09:37 -0200

    modules/dbe_wishbone/*: add (x)wb_bpm_pcie components
    Previously, these components were declared in a
    FPGA-specific package like "bpm_pcie_a7_pkg".

--- hdl/modules/dbe_wishbone/dbe_wishbone_pkg.vhd
+++ hdl/modules/dbe_wishbone/dbe_wishbone_pkg.vhd
@@ -8,6 +8,8 @@ use work.wb_stream_generic_pkg.all;
 use work.fmc_adc_pkg.all;
 use work.wr_fabric_pkg.all;
 use work.acq_core_pkg.all;
+use work.ipcores_pkg.all;
+use work.bpm_axi_pkg.all;
 
 package dbe_wishbone_pkg is
 
@@ -1660,6 +1662,157 @@ package dbe_wishbone_pkg is
     spi_si                                    : in  std_logic;
     spi_so                                    : out std_logic;
     spi_clk                                   : in  std_logic
+  );
+  end component;
+
+  component wb_bpm_pcie
+  generic (
+    g_ma_interface_mode                       : t_wishbone_interface_mode := PIPELINED;
+    g_ma_address_granularity                  : t_wishbone_address_granularity := BYTE;
+    g_simulation                              : string  := "FALSE"
+  );
+  port (
+    -- DDR3 memory pins
+    ddr3_dq_b                                 : inout std_logic_vector(c_ddr_dq_width-1 downto 0);
+    ddr3_dqs_p_b                              : inout std_logic_vector(c_ddr_dqs_width-1 downto 0);
+    ddr3_dqs_n_b                              : inout std_logic_vector(c_ddr_dqs_width-1 downto 0);
+    ddr3_addr_o                               : out   std_logic_vector(c_ddr_row_width-1 downto 0);
+    ddr3_ba_o                                 : out   std_logic_vector(c_ddr_bank_width-1 downto 0);
+    ddr3_cs_n_o                               : out   std_logic_vector(0 downto 0);
+    ddr3_ras_n_o                              : out   std_logic;
+    ddr3_cas_n_o                              : out   std_logic;
+    ddr3_we_n_o                               : out   std_logic;
+    ddr3_reset_n_o                            : out   std_logic;
+    ddr3_ck_p_o                               : out   std_logic_vector(c_ddr_ck_width-1 downto 0);
+    ddr3_ck_n_o                               : out   std_logic_vector(c_ddr_ck_width-1 downto 0);
+    ddr3_cke_o                                : out   std_logic_vector(c_ddr_cke_width-1 downto 0);
+    ddr3_dm_o                                 : out   std_logic_vector(c_ddr_dm_width-1 downto 0);
+    ddr3_odt_o                                : out   std_logic_vector(c_ddr_odt_width-1 downto 0);
+
+    -- PCIe transceivers
+    pci_exp_rxp_i                             : in  std_logic_vector(c_pcielanes - 1 downto 0);
+    pci_exp_rxn_i                             : in  std_logic_vector(c_pcielanes - 1 downto 0);
+    pci_exp_txp_o                             : out std_logic_vector(c_pcielanes - 1 downto 0);
+    pci_exp_txn_o                             : out std_logic_vector(c_pcielanes - 1 downto 0);
+
+    -- Necessity signals
+    ddr_clk_i                                 : in std_logic; --200 MHz DDR core clock (connect through BUFG or PLL)
+    ddr_rst_i                                 : in std_logic; --200 MHz DDR core clock (connect through BUFG or PLL)
+    pcie_clk_p_i                              : in std_logic; --100 MHz PCIe Clock (connect directly to input pin)
+    pcie_clk_n_i                              : in std_logic; --100 MHz PCIe Clock
+    pcie_rst_n_i                              : in std_logic; --Reset to PCIe core
+
+    -- DDR memory controller interface --
+    ddr_aximm_sl_aclk_o                       : out std_logic;
+    ddr_aximm_sl_aresetn_o                    : out std_logic;
+    ddr_aximm_sl_awid_i                       : in std_logic_vector (0 downto 0);
+    ddr_aximm_sl_awaddr_i                     : in std_logic_vector (31 downto 0);
+    ddr_aximm_sl_awlen_i                      : in std_logic_vector (7 downto 0);
+    ddr_aximm_sl_awsize_i                     : in std_logic_vector (2 downto 0);
+    ddr_aximm_sl_awburst_i                    : in std_logic_vector (1 downto 0);
+    ddr_aximm_sl_awlock_i                     : in std_logic;
+    ddr_aximm_sl_awcache_i                    : in std_logic_vector (3 downto 0);
+    ddr_aximm_sl_awprot_i                     : in std_logic_vector (2 downto 0);
+    ddr_aximm_sl_awqos_i                      : in std_logic_vector (3 downto 0);
+    ddr_aximm_sl_awvalid_i                    : in std_logic;
+    ddr_aximm_sl_awready_o                    : out std_logic;
+    ddr_aximm_sl_wdata_i                      : in std_logic_vector (c_ddr_payload_width-1 downto 0);
+    ddr_aximm_sl_wstrb_i                      : in std_logic_vector (c_ddr_payload_width/8-1 downto 0);
+    ddr_aximm_sl_wlast_i                      : in std_logic;
+    ddr_aximm_sl_wvalid_i                     : in std_logic;
+    ddr_aximm_sl_wready_o                     : out std_logic;
+    ddr_aximm_sl_bready_i                     : in std_logic;
+    ddr_aximm_sl_bid_o                        : out std_logic_vector (0 downto 0);
+    ddr_aximm_sl_bresp_o                      : out std_logic_vector (1 downto 0);
+    ddr_aximm_sl_bvalid_o                     : out std_logic;
+    ddr_aximm_sl_arid_i                       : in std_logic_vector (0 downto 0);
+    ddr_aximm_sl_araddr_i                     : in std_logic_vector (31 downto 0);
+    ddr_aximm_sl_arlen_i                      : in std_logic_vector (7 downto 0);
+    ddr_aximm_sl_arsize_i                     : in std_logic_vector (2 downto 0);
+    ddr_aximm_sl_arburst_i                    : in std_logic_vector (1 downto 0);
+    ddr_aximm_sl_arlock_i                     : in std_logic;
+    ddr_aximm_sl_arcache_i                    : in std_logic_vector (3 downto 0);
+    ddr_aximm_sl_arprot_i                     : in std_logic_vector (2 downto 0);
+    ddr_aximm_sl_arqos_i                      : in std_logic_vector (3 downto 0);
+    ddr_aximm_sl_arvalid_i                    : in std_logic;
+    ddr_aximm_sl_arready_o                    : out std_logic;
+    ddr_aximm_sl_rready_i                     : in std_logic;
+    ddr_aximm_sl_rid_o                        : out std_logic_vector (0 downto 0 );
+    ddr_aximm_sl_rdata_o                      : out std_logic_vector (c_ddr_payload_width-1 downto 0);
+    ddr_aximm_sl_rresp_o                      : out std_logic_vector (1 downto 0 );
+    ddr_aximm_sl_rlast_o                      : out std_logic;
+    ddr_aximm_sl_rvalid_o                     : out std_logic;
+
+    -- Wishbone interface --
+    wb_clk_i                                  : in std_logic;
+    wb_rst_i                                  : in std_logic;
+    wb_ma_adr_o                               : out std_logic_vector(c_wishbone_address_width-1 downto 0);
+    wb_ma_dat_o                               : out std_logic_vector(c_wishbone_data_width-1 downto 0);
+    wb_ma_sel_o                               : out std_logic_vector(c_wishbone_data_width/8-1 downto 0);
+    wb_ma_cyc_o                               : out std_logic;
+    wb_ma_stb_o                               : out std_logic;
+    wb_ma_we_o                                : out std_logic;
+    wb_ma_dat_i                               : in  std_logic_vector(c_wishbone_data_width-1 downto 0)    := cc_dummy_data;
+    wb_ma_err_i                               : in  std_logic                                             := '0';
+    wb_ma_rty_i                               : in  std_logic                                             := '0';
+    wb_ma_ack_i                               : in  std_logic                                             := '0';
+    wb_ma_stall_i                             : in  std_logic                                             := '0';
+    -- Additional exported signals for instantiation
+    wb_ma_pcie_rst_o                          : out std_logic
+  );
+  end component;
+
+  component xwb_bpm_pcie
+  generic (
+    g_ma_interface_mode                       : t_wishbone_interface_mode := PIPELINED;
+    g_ma_address_granularity                  : t_wishbone_address_granularity := BYTE;
+    g_simulation                              : string  := "FALSE"
+  );
+  port (
+    -- DDR3 memory pins
+    ddr3_dq_b                                 : inout std_logic_vector(c_ddr_dq_width-1 downto 0);
+    ddr3_dqs_p_b                              : inout std_logic_vector(c_ddr_dqs_width-1 downto 0);
+    ddr3_dqs_n_b                              : inout std_logic_vector(c_ddr_dqs_width-1 downto 0);
+    ddr3_addr_o                               : out   std_logic_vector(c_ddr_row_width-1 downto 0);
+    ddr3_ba_o                                 : out   std_logic_vector(c_ddr_bank_width-1 downto 0);
+    ddr3_cs_n_o                               : out   std_logic_vector(0 downto 0);
+    ddr3_ras_n_o                              : out   std_logic;
+    ddr3_cas_n_o                              : out   std_logic;
+    ddr3_we_n_o                               : out   std_logic;
+    ddr3_reset_n_o                            : out   std_logic;
+    ddr3_ck_p_o                               : out   std_logic_vector(c_ddr_ck_width-1 downto 0);
+    ddr3_ck_n_o                               : out   std_logic_vector(c_ddr_ck_width-1 downto 0);
+    ddr3_cke_o                                : out   std_logic_vector(c_ddr_cke_width-1 downto 0);
+    ddr3_dm_o                                 : out   std_logic_vector(c_ddr_dm_width-1 downto 0);
+    ddr3_odt_o                                : out   std_logic_vector(c_ddr_odt_width-1 downto 0);
+
+    -- PCIe transceivers
+    pci_exp_rxp_i                             : in  std_logic_vector(c_pcielanes - 1 downto 0);
+    pci_exp_rxn_i                             : in  std_logic_vector(c_pcielanes - 1 downto 0);
+    pci_exp_txp_o                             : out std_logic_vector(c_pcielanes - 1 downto 0);
+    pci_exp_txn_o                             : out std_logic_vector(c_pcielanes - 1 downto 0);
+
+    -- Necessity signals
+    ddr_clk_i                                 : in std_logic; --200 MHz DDR core clock (connect through BUFG or PLL)
+    ddr_rst_i                                 : in std_logic; --200 MHz DDR core clock (connect through BUFG or PLL)
+    pcie_clk_p_i                              : in std_logic; --100 MHz PCIe Clock (connect directly to input pin)
+    pcie_clk_n_i                              : in std_logic; --100 MHz PCIe Clock
+    pcie_rst_n_i                              : in std_logic; --Reset to PCIe core
+
+    -- DDR memory controller interface --
+    ddr_aximm_sl_aclk_o                       : out std_logic;
+    ddr_aximm_sl_aresetn_o                    : out std_logic;
+    ddr_aximm_sl_i                            : in t_aximm_in := cc_dummy_aximm_in;
+    ddr_aximm_sl_o                            : out t_aximm_out;
+
+    -- Wishbone interface --
+    wb_clk_i                                  : in std_logic;
+    wb_rst_i                                  : in std_logic;
+    wb_ma_i                                   : in  t_wishbone_master_in := cc_dummy_slave_out;
+    wb_ma_o                                   : out t_wishbone_master_out;
+
+    -- Additional exported signals for instantiation
+    wb_ma_pcie_rst_o                          : out std_logic
   );
   end component;
 
