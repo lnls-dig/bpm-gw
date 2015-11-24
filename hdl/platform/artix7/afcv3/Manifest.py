2221bcdedfe709cea12022406efb927183294861
Lucas Russo <lerwys@gmail.com>
2015-11-24 16:55:36 -0200

    platform/artix7/afcv3/Manifst.py: include new BPM AXI cores

--- hdl/platform/artix7/afcv3/Manifest.py
+++ hdl/platform/artix7/afcv3/Manifest.py
@@ -3,5 +3,7 @@ files = ["ipcores_pkg.vhd"]
 modules = {"local" : ["pcie_core",
                     "ddr_core",
                     "axi_datamover_0",
-                    "axi_interconnect"]}
+                    "axi_datamover_bpm",
+                    "axi_interconnect",
+                    "axi_interconnect_bpm"]}
 
