6f9b548aedb007909e5d87f78cf24133d24b52f5
Vfinotti <vfinotti@gmail.com>
2015-12-15 14:59:29 -0200

    Updated Manifest.py to remove "sm_next" and "sm_state" and added "sm_stated_rcv"

--- hdl/top/afc_v3/vivado/test_trigger/rcv/Manifest.py
+++ hdl/top/afc_v3/vivado/test_trigger/rcv/Manifest.py
@@ -1,8 +1,6 @@
 files = [ "test_trigger_rcv.vhd",
-          "test_trigger_rcv.xdc",
 	  "sm_counter.vhd",
-	  "sm_next.vhd",
-	  "sm_state.vhd",
+	  "sm_states_rcv.vhd",
           "sys_pll.vhd",
           "clk_gen.vhd"];
 
