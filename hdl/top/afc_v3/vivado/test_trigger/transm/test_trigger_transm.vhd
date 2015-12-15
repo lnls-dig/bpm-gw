8efeeadf311b954303364f531951a58cd23f1d20
Vfinotti <vfinotti@gmail.com>
2015-12-15 15:54:51 -0200

    Changes signals in components instantiations on test_trigger_transm.vhd for the correct ones.

--- hdl/top/afc_v3/vivado/test_trigger/transm/test_trigger_transm.vhd
+++ hdl/top/afc_v3/vivado/test_trigger/transm/test_trigger_transm.vhd
@@ -6,7 +6,7 @@
 -- Author     : Vitor Finotti Ferreira  <vfinotti@finotti-Inspiron-7520>
 -- Company    : Brazilian Synchrotron Light Laboratory, LNLS/CNPEM
 -- Created    : 2015-12-09
--- Last update: 2015-12-09
+-- Last update: 2015-12-15
 -- Platform   :
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -40,9 +40,9 @@ use ieee.numeric_std.all;
 entity test_trigger_transm is
 
   port (
-    sys_clk_p_i : in std_logic;
-    sys_clk_n_i : in std_logic;
-    trigger_o   : in std_logic_vector(7 downto 0));
+    sys_clk_p_i : in  std_logic;
+    sys_clk_n_i : in  std_logic;
+    trigger_o   : out std_logic_vector(7 downto 0));
 
 end entity test_trigger_transm;
 
@@ -56,30 +56,47 @@ architecture structure of test_trigger_transm is
   signal trigger_buf : std_logic_vector(7 downto 0);
   signal pulse       : std_logic_vector(7 downto 0);
 
+  type count_array is array(7 downto 0) of std_logic_vector(c_count_width-1 downto 0);
+  signal count_sent : count_array;
+
   constant filler : std_logic_vector(31 downto 0) := (others => '0');
 
   component sm_transm is
     generic (
       g_num_pins         : natural;
-      g_cycles_to_change : natural);
+      g_max_pulse_length : natural);
+    port (
+      clk_i              : in  std_logic;
+      rst_n_i            : in  std_logic;
+      cycles_to_change_i : in  natural;
+      pulse_o            : out std_logic_vector(g_num_pins-1 downto 0));
+  end component sm_transm;
+
+  component counter_simple is
+    generic (
+      g_output_width : positive);
     port (
       clk_i   : in  std_logic;
       rst_n_i : in  std_logic;
-      pulse_o : out std_logic_vector(g_num_pins-1 downto 0));
-  end component sm_transm;
+      ce_i    : in  std_logic;
+      up_i    : in  std_logic;
+      down_i  : in  std_logic;
+      count_o : out std_logic_vector(g_output_width-1 downto 0));
+  end component counter;
 
 -------------------------------------------------------------------------------
 -- Chipscope
 -------------------------------------------------------------------------------
 
-  signal CONTROL0, CONTROL1 : std_logic_vector(35 downto 0);
+  signal CONTROL0, CONTROL1, CONTROL2, CONTROL3 : std_logic_vector(35 downto 0);
 
   component chipscope_icon_4_port is
     port (
       CONTROL0 : inout std_logic_vector(35 downto 0);
       CONTROL1 : inout std_logic_vector(35 downto 0);
       CONTROL2 : inout std_logic_vector(35 downto 0);
-      CONTROL3 : inout std_logic_vector(35 downto 0));
+      CONTROL3 : inout std_logic_vector(35 downto 0)
+      );
   end component chipscope_icon_4_port;
 
   component chipscope_ila is
@@ -92,7 +109,7 @@ architecture structure of test_trigger_transm is
       TRIG3   : in    std_logic_vector(31 downto 0));
   end component chipscope_ila;
 
-  component chipscope_vio_31 is
+  component chipscope_vio_32 is
     port (
       CONTROL  : inout std_logic_vector(35 downto 0);
       CLK      : in    std_logic;
@@ -131,8 +148,11 @@ architecture structure of test_trigger_transm is
   signal sys_clk_gen_bufg       : std_logic;
   signal locked                 : std_logic;
   signal rst                    : std_logic;
+  signal rst_n                  : std_logic;
 
 begin  -- architecture behav
+
+  trigger_o <= pulse;
 
   -- Clock generation
   cmp_clk_gen : clk_gen
@@ -146,11 +166,27 @@ begin  -- architecture behav
   sm_transm_1 : sm_transm
     generic map (
       g_num_pins         => c_glitch_len_width,
-      g_cycles_to_change => 10)
+      g_max_pulse_length => 10000)
     port map (
-      clk_i   => clk_100mhz,
-      rst_n_i => rst_n_i,
-      pulse_o => trigger_o);
+      clk_i              => clk_100mhz,
+      rst_n_i            => rst_n,
+      cycles_to_change_i => to_integer(unsigned(length)),
+      pulse_o            => pulse);
+
+  generate_counter : for i in c_glitch_len_width-1 downto 0 generate
+
+    counter_simplei : counter_simple
+      generic map (
+        g_output_width => c_count_width)
+      port map (
+        clk_i   => clk_100mhz,
+        rst_n_i => rst_n,
+        ce_i    => '1',
+        up_i    => pulse(i),
+        down_i  => '0',
+        count_o => count_sent(i));
+
+  end generate generate_counter;
 
   -- Obtain core locking and generate necessary clocks
   cmp_sys_pll_inst : sys_pll
@@ -170,30 +206,50 @@ begin  -- architecture behav
       clk_i    => sys_clk_gen_bufg,
       clk0_o   => clk_100mhz,           -- 100MHz locked clock
       clk1_o   => clk_200mhz,           -- 200MHz locked clock
-      locked_o => rst                   -- '1' when the PLL has locked
+      locked_o => rst_n                 -- '1' when the PLL has locked
       );
 
   cmp_chipscope_icon_4_port : chipscope_icon_4_port
     port map (
       CONTROL0 => CONTROL0,
-      CONTROL1 => CONTROL1);
+      CONTROL1 => CONTROL1,
+      CONTROL2 => CONTROL2,
+      CONTROL3 => CONTROL3);
 
   cmp_chipscope_ila_0 : entity work.chipscope_ila
     port map (
       CONTROL            => CONTROL0,
       CLK                => clk_100mhz,
-      TRIG0(7 downto 0)  => trigger_o,
-      TRIG0(31 downto 8) => filler(31 downto 8),
+      TRIG0(7 downto 0)  => pulse,
+      TRIG0(8)           => clk_100mhz,
+      TRIG0(31 downto 9) => filler(31 downto 9),
       TRIG1              => filler,
       TRIG2              => filler,
       TRIG3              => filler);
+  cmp_chipscope_ila_2 : entity work.chipscope_ila
+    port map (
+      CONTROL => CONTROL2,
+      CLK     => clk_100mhz,
+      TRIG0   => filler,
+      TRIG1   => filler,
+      TRIG2   => filler,
+      TRIG3   => filler);
+  cmp_chipscope_ila_3 : entity work.chipscope_ila
+    port map (
+      CONTROL => CONTROL3,
+      CLK     => clk_100mhz,
+      TRIG0   => filler,
+      TRIG1   => filler,
+      TRIG2   => filler,
+      TRIG3   => filler);
 
   cmp_chipscope_vio : entity work.chipscope_vio_32
     port map (
       CONTROL                => CONTROL1,
       CLK                    => clk_100mhz,
       SYNC_OUT(7 downto 0)   => length,
-      SYNC_OUT(15 downto 8)  => direction,
+      SYNC_OUT(15 downto 8)  => open,
       SYNC_OUT(31 downto 16) => open);
+
 
 end architecture structure;
