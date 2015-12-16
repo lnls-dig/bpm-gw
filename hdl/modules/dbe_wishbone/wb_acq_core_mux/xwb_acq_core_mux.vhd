4b0ad8e007feda7b8d2490519b37b7736f39ad2e
Lucas Russo <lerwys@gmail.com>
2015-12-16 17:33:42 -0200

    modules/*/wb_acq_core_mux/xwb*: use new BURST and DDR3_INTERFACE generics

--- hdl/modules/dbe_wishbone/wb_acq_core_mux/xwb_acq_core_mux.vhd
+++ hdl/modules/dbe_wishbone/wb_acq_core_mux/xwb_acq_core_mux.vhd
@@ -52,7 +52,9 @@ generic
   g_multishot_ram_size                      : natural := 2048;
   g_fifo_fc_size                            : natural := 64;
   g_sim_readback                            : boolean := false;
-  g_acq_num_cores                           : natural := 2
+  g_acq_num_cores                           : natural := 2;
+  g_ddr_interface_type                      : string  := "AXIS";
+  g_max_burst_size                          : natural := 4
 );
 port
 (
@@ -152,7 +154,9 @@ begin
     g_ddr_dq_width                           => g_ddr_dq_width,
     g_multishot_ram_size                     => g_multishot_ram_size,
     g_fifo_fc_size                           => g_fifo_fc_size,
-    g_sim_readback                           => g_sim_readback
+    g_sim_readback                           => g_sim_readback,
+    g_ddr_interface_type                     => g_ddr_interface_type,
+    g_max_burst_size                         => g_max_burst_size
   )
   port map
   (
