a65696a22805409f371df15cfe0c5c34b8702ee1
Vfinotti <vfinotti@gmail.com>
2015-12-18 09:43:13 -0200

    Included pulse extender file on the Manifest.py of the transmitter

--- hdl/top/afc_v3/vivado/test_trigger/transm/Manifest.py
+++ hdl/top/afc_v3/vivado/test_trigger/transm/Manifest.py
@@ -1,7 +1,8 @@
 files = [ "test_trigger_transm.vhd",
 	  "sm_transm.vhd",
           "sys_pll.vhd",
-          "clk_gen.vhd"];
+          "clk_gen.vhd",
+	  "extend_pulse_dyn.vhd"];
 
 modules = { "local" :
              ["../../../../.."]
