a7f7c02f44b5d6109e057ec5a6fbad30bfbf7e7c
Vfinotti <vfinotti@gmail.com>
2016-01-27 16:02:03 -0200

    Created testbench files for wb_mlvds_trigger.vhd

---
+++ hdl/testbench/wishbone/wb_mlvds_trigger/Manifest.py
@@ -0,0 +1,13 @@
+action = "simulation"
+sim_tool = "modelsim"
+
+top_module = "wb_mlvds_trigger_tb"
+machine_pkg = "uvx_130M"
+
+target = "xilinx"
+syn_device = "xc7a200t"
+
+modules = {"local" : ["../../../"]}
+
+files = ["wb_mlvds_trigger_tb.vhd"]	
+
