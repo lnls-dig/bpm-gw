4098e133c624054d01165c7c19e6b39a2e75a4e6
Lucas Russo <lerwys@gmail.com>
2016-02-16 12:25:45 -0200

    platform/artix7/*: add new ac_701 design files

---
+++ hdl/platform/artix7/ac_701/axi_datamover_0/Manifest.py
@@ -0,0 +1,12 @@
+if (action == "synthesis"):
+    files = ["axi_datamover_0.xci"]
+
+else:
+    files = ["axi_datamover_v5_1/hdl/src/vhdl/",
+            "blk_mem_gen_v8_2/hdl/blk_mem_gen_v8_2.vhd",
+            "fifo_generator_v12_0/simulation/",
+            "lib_cdc_v1_0/hdl/src/vhdl/",
+            "lib_fifo_v1_0/hdl/src/vhdl/",
+            "lib_pkg_v1_0/hdl/src/vhdl/",
+            "lib_srl_fifo_v1_0/hdl/src/vhdl/",
+            "sim/"]
