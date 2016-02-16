61df2af86b617c07c4c8b9423652748e1e4e61fd
Lucas Russo <lerwys@gmail.com>
2016-02-16 12:26:36 -0200

    top/*: add new ac_701 top files

---
+++ hdl/top/ac_701/vivado/dbe_bpm/Manifest.py
@@ -0,0 +1,13 @@
+files = [ "dbe_bpm.vhd",
+          "sys_pll.vhd",
+          "clk_gen.vhd",
+          "dbe_bpm.xdc",
+          "pcie_core.xdc",
+          "ddr_core.xdc",
+          "dbe_bpm.xcf" ];
+
+modules = { "local" :
+             ["../../../.."
+             ]
+          };
+
