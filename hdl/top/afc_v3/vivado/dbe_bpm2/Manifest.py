d3a86e1b74a8663eb52df9558428ae45e627b09a
Lucas Russo <lerwys@gmail.com>
2016-02-19 19:56:45 -0200

    top/afc_v3/*: add new top design with fmc250M board

---
+++ hdl/top/afc_v3/vivado/dbe_bpm2/Manifest.py
@@ -0,0 +1,13 @@
+files = [ "dbe_bpm2.vhd",
+          "sys_pll.vhd",
+          "clk_gen.vhd",
+          "dbe_bpm2.xdc",
+          "pcie_core.xdc",
+          "ddr_core.xdc",
+          "dbe_bpm2.xcf" ];
+
+modules = { "local" :
+             ["../../../.."
+             ]
+          };
+
