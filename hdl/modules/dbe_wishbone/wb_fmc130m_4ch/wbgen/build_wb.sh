e72a13a4a182fd351a1c6bf92a0e264f4827503c
Lucas Russo <lerwys@gmail.com>
2016-04-18 08:58:00 -0300

    wb_fmc130m_4ch: prefix generated Wishbone .h file with "wb_"

--- hdl/modules/dbe_wishbone/wb_fmc130m_4ch/wbgen/build_wb.sh
+++ hdl/modules/dbe_wishbone/wb_fmc130m_4ch/wbgen/build_wb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wbgen2 -V fmc_130m_4ch_regs.vhd -H record -p fmc_130m_4ch_regs_pkg.vhd -K ../../../../sim/regs/wb_fmc130m_4ch_regs.vh -s defines -C fmc130m_4ch_regs.h -f html -D doc/fmc130m_4ch_regs_wb.html fmc_130m_4ch_regs.wb
+wbgen2 -V fmc_130m_4ch_regs.vhd -H record -p fmc_130m_4ch_regs_pkg.vhd -K ../../../../sim/regs/wb_fmc130m_4ch_regs.vh -s defines -C wb_fmc130m_4ch_regs.h -f html -D doc/fmc130m_4ch_regs_wb.html fmc_130m_4ch_regs.wb
