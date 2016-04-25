f0abe4eb350a2578137f1d455722bb51bff47d1f
Vitor F. Ferreira <vfinotti@gmail.com>
2016-04-25 15:32:35 -0300

    Fixed constraints file extension on test_trigger/rcv/Manifest.py

--- hdl/top/afc_v3/vivado/test_trigger/rcv/Manifest.py
+++ hdl/top/afc_v3/vivado/test_trigger/rcv/Manifest.py
@@ -3,9 +3,8 @@ files = [ "test_trigger_rcv.vhd",
 	  "sm_states_rcv.vhd",
           "sys_pll.vhd",
           "clk_gen.vhd",
-	  "test_trigger_rcv.xcd"];
+	  "test_trigger_rcv.xdc"];
 
 modules = { "local" :
              ["../../../../.."]
           };
-
