34509efed1bde4fb774e494e0562a3232d211b71
Vitor F. Ferreira <vfinotti@gmail.com>
2016-04-27 12:56:11 -0300

    Removed "extend_pulse_dyn", "trigger_rcv" and "counter_simple" component declarations from wb_trigger (they are at dbe_common_pkg)

--- hdl/modules/dbe_wishbone/wb_trigger/wb_trigger.vhd
+++ hdl/modules/dbe_wishbone/wb_trigger/wb_trigger.vhd
@@ -129,45 +129,8 @@ architecture rtl of wb_trigger is
       regs_o     : out t_wb_trig_out_registers);
   end component wb_slave_trigger;
 
-  component extend_pulse_dyn is
-    generic (
-      g_width_bus_size : natural);
-    port (
-      clk_i         : in  std_logic;
-      rst_n_i       : in  std_logic;
-      pulse_i       : in  std_logic;
-      pulse_width_i : in  unsigned(g_width_bus_size-1 downto 0);
-      extended_o    : out std_logic := '0');
-  end component extend_pulse_dyn;
-
-  component trigger_rcv is
-    generic (
-      g_glitch_len_width : positive;
-      g_sync_edge        : string);
-    port (
-      clk_i   : in  std_logic;
-      rst_n_i : in  std_logic;
-      len_i   : in  std_logic_vector(g_glitch_len_width-1 downto 0);
-      data_i  : in  std_logic;
-      pulse_o : out std_logic);
-  end component trigger_rcv;
-
-  component counter_simple is
-    generic (
-      g_output_width : positive);
-    port (
-      clk_i   : in  std_logic;
-      rst_n_i : in  std_logic;
-      ce_i    : in  std_logic;
-      up_i    : in  std_logic;
-      down_i  : in  std_logic;
-      count_o : out std_logic_vector(g_output_width-1 downto 0));
-  end component counter_simple;
-
-
 
   constant c_periph_addr_size : natural := 6+2;
-
 
 
   -----------
