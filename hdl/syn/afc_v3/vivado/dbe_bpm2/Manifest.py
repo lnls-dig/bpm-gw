5c594ec0bd0d211a0cfff7213295157cc71661cc
Lucas Russo <lerwys@gmail.com>
2016-05-03 16:19:59 -0300

    syn/afc_v3/vivado/dbe_bpm2: use correct machine version UVX_250M

--- hdl/syn/afc_v3/vivado/dbe_bpm2/Manifest.py
+++ hdl/syn/afc_v3/vivado/dbe_bpm2/Manifest.py
@@ -10,9 +10,7 @@ syn_tool = "vivado"
 
 files = ["synthesis_descriptor_pkg.vhd"];
 
-# FIXME. We are using the 130M DSP CORES, as we will test it with
-# 130 MHz
-machine_pkg = "uvx_130M";
+machine_pkg = "uvx_250M";
 
 modules = { "local" : [ "../../../../top/afc_v3/vivado/dbe_bpm2" ] };
 
