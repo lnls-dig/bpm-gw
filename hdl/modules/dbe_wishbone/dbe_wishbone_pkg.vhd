65f32542925fbd3911b5d08e849ea977e803ae55
Lucas Russo <lerwys@gmail.com>
2016-05-19 14:23:22 -0300

    modules/*/dbe_wishbone_pkg.vhd: fix trigger_iface/mux addressing space
    The module uses 9 bits with BYTE addressing. So we need
    at lest 0x1FF addressing mask. For precaution, increase
    trigger_mux addressing.

--- hdl/modules/dbe_wishbone/dbe_wishbone_pkg.vhd
+++ hdl/modules/dbe_wishbone/dbe_wishbone_pkg.vhd
@@ -2729,7 +2729,7 @@ package dbe_wishbone_pkg is
     wbd_width     => x"7",                     -- 8/16/32-bit port granularity (0111)
     sdb_component => (
     addr_first    => x"0000000000000000",
-    addr_last     => x"00000000000000FF",
+    addr_last     => x"00000000000003FF",
     product => (
     vendor_id     => x"1000000000001215",     -- LNLS
     device_id     => x"84b6a5ac",
@@ -2746,7 +2746,7 @@ package dbe_wishbone_pkg is
     wbd_width     => x"7",                     -- 8/16/32-bit port granularity (0111)
     sdb_component => (
     addr_first    => x"0000000000000000",
-    addr_last     => x"00000000000000FF",
+    addr_last     => x"00000000000003FF",
     product => (
     vendor_id     => x"1000000000001215",     -- LNLS
     device_id     => x"bcbb78d2",
