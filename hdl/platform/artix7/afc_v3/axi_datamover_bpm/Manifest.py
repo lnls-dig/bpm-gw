62a845eb79d434f2476649cb917c2ce913b563ba
Lucas Russo <lerwys@gmail.com>
2016-07-08 16:51:15 -0300

    platform/artix7/*: remove Xilinx libraries from Manifest
    They should be compiled separately, through scripts
    such as the one available in
    "hdl/testbench/wishbone/wb_acq_core_test/verilog/
    artix7/full_tb_mux/questa_compile.sh".
    
    This is only necessary as some of these libraries contains
    encrypted files and can't be compiled externally (e.g., modelsim),
    giving us a warning: "memory in protected region".

--- hdl/platform/artix7/afc_v3/axi_datamover_bpm/Manifest.py
+++ hdl/platform/artix7/afc_v3/axi_datamover_bpm/Manifest.py
@@ -3,10 +3,10 @@ if (action == "synthesis"):
 
 else:
     files = ["axi_datamover_v5_1/hdl/src/vhdl/",
-            "blk_mem_gen_v8_2/hdl/blk_mem_gen_v8_2.vhd",
-            "fifo_generator_v12_0/simulation/",
-            "lib_cdc_v1_0/hdl/src/vhdl/",
-            "lib_fifo_v1_0/hdl/src/vhdl/",
-            "lib_pkg_v1_0/hdl/src/vhdl/",
-            "lib_srl_fifo_v1_0/hdl/src/vhdl/",
+#            "blk_mem_gen_v8_2/hdl/blk_mem_gen_v8_2.vhd",
+#            "fifo_generator_v12_0/simulation/",
+#            "lib_cdc_v1_0/hdl/src/vhdl/",
+#            "lib_fifo_v1_0/hdl/src/vhdl/",
+#            "lib_pkg_v1_0/hdl/src/vhdl/",
+#            "lib_srl_fifo_v1_0/hdl/src/vhdl/",
             "sim/"]
--- hdl/platform/artix7/afc_v3/axi_interconnect/Manifest.py
+++ hdl/platform/artix7/afc_v3/axi_interconnect/Manifest.py
@@ -3,6 +3,6 @@ if (action == "synthesis"):
 
 else:
     files = ["axi_interconnect_v1_7/hdl/verilog/",
-            "blk_mem_gen_v8_2/hdl/blk_mem_gen_v8_2.vhd",
+#            "blk_mem_gen_v8_2/hdl/blk_mem_gen_v8_2.vhd",
             "fifo_generator_v12_0/simulation/",
             "sim/"]
