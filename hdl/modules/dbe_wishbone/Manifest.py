a6acddf5f4c1e6d1e8c3a097e6d83704992a63f1
Lucas Russo <lerwys@gmail.com>
2016-09-25 11:56:18 -0300

    modules/dbe_wishbone/Manifest.py: add missing afc_diag module

--- hdl/modules/dbe_wishbone/Manifest.py
+++ hdl/modules/dbe_wishbone/Manifest.py
@@ -5,6 +5,7 @@ modules = { "local" : [
                         "wb_trigger_iface",
                         "wb_trigger_mux",
                         "wb_trigger",
+                        "wb_afc_diag",
                         "wb_fmc150",
                         "wb_fmc516",
                         "wb_fmc130m_4ch",
