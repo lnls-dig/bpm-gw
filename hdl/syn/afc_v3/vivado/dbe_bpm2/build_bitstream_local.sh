0641d262ba729796dd6e9e1a21b8427cfc5ebfde
Lucas Russo <lerwys@gmail.com>
2016-09-26 09:09:02 -0300

    syn/afc_v3/vivado/*: remove synthesis SDB build on script
    This is already called by the generated
    makefile. So, we don't need to call it twice.

--- hdl/syn/afc_v3/vivado/dbe_bpm2/build_bitstream_local.sh
+++ hdl/syn/afc_v3/vivado/dbe_bpm2/build_bitstream_local.sh
@@ -5,8 +5,6 @@ set -e
 # Check for uninitialized variables
 set -u
 
-./build_synthesis_sdb.sh
-
 COMMAND="(time make; date) 2>&1 | tee make_output &"
 
 echo $COMMAND
--- hdl/syn/afc_v3/vivado/dbe_bpm2/build_bitstream_remote.sh
+++ hdl/syn/afc_v3/vivado/dbe_bpm2/build_bitstream_remote.sh
@@ -5,8 +5,6 @@ set -e
 # Check for uninitialized variables
 set -u
 
-./build_synthesis_sdb.sh
-
 COMMAND="(hdlmake; make cleanremote; time make remote; make sync; date) 2>&1 | tee make_output &"
 
 echo $COMMAND
