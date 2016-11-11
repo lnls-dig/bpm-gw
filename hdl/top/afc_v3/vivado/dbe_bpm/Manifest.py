3fd1077259176270dc9dda80852bec08a59bf2da
Lucas Russo <lerwys@gmail.com>
2016-11-11 10:44:54 -0200

    top/afc_v3/*: unify FMC130 and FMC250 top designs
    Now, both designs use the generic
    dbe_bpm_gen top design.

--- hdl/top/afc_v3/vivado/dbe_bpm/Manifest.py
+++ hdl/top/afc_v3/vivado/dbe_bpm/Manifest.py
@@ -1,13 +1,12 @@
 files = [ "dbe_bpm.vhd",
-          "sys_pll.vhd",
-          "clk_gen.vhd",
           "dbe_bpm.xdc",
           "pcie_core.xdc",
           "ddr_core.xdc",
           "dbe_bpm.xcf" ];
 
 modules = { "local" :
-             ["../../../.."
+             ["../../../..",
+              "../dbe_bpm_gen"
              ]
           };
 
