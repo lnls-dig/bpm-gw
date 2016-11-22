f1849bfc983bee6ec8e7137dd091b751af3a17af
Lucas Russo <lerwys@gmail.com>
2016-11-22 15:51:40 -0200

    syn/afc_v3/*: add missing Vivado properties to Manifest.py

--- hdl/syn/afc_v3/vivado/dbe_bpm/Manifest.py
+++ hdl/syn/afc_v3/vivado/dbe_bpm/Manifest.py
@@ -11,14 +11,17 @@ syn_properties = [
     ["steps.synth_design.args.more options", "-verbose"],
     ["steps.opt_design.args.verbose", "1"],
     ["steps.opt_design.args.directive", "Explore"],
+    ["steps.opt_design.is_enabled", "1"],
     ["steps.place_design.args.directive", "Explore"],
     ["steps.place_design.args.more options", "-verbose"],
     ["steps.phys_opt_design.args.directive", "AlternateFlowWithRetiming"],
     ["steps.phys_opt_design.args.more options", "-verbose"],
+    ["steps.phys_opt_design.is_enabled", "1"],
     ["steps.route_design.args.directive", "Explore"],
     ["steps.route_design.args.more options", "-verbose"],
     ["steps.post_route_phys_opt_design.args.directive", "AddRetime"],
     ["steps.post_route_phys_opt_design.args.more options", "-verbose"],
+    ["steps.post_route_phys_opt_design.is_enabled", "1"],
     ["steps.write_bitstream.args.verbose", "1"]]
 
 import os
