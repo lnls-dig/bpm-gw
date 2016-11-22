8e6550de4ef8b5d50d2187cef08ac5cf65c019fe
Lucas Russo <lerwys@gmail.com>
2016-11-22 12:01:01 -0200

    syn/afc_v3/*: add synth/impl Vivado properties to Manifest.py
    hdlmake release-3.0 commit 034553408a
    implements this new feature.

--- hdl/syn/afc_v3/vivado/dbe_bpm/Manifest.py
+++ hdl/syn/afc_v3/vivado/dbe_bpm/Manifest.py
@@ -7,6 +7,19 @@ syn_package = "ffg1156"
 syn_top = "dbe_bpm"
 syn_project = "dbe_bpm"
 syn_tool = "vivado"
+syn_properties = [
+    ["steps.synth_design.args.more options", "-verbose"],
+    ["steps.opt_design.args.verbose", "1"],
+    ["steps.opt_design.args.directive", "Explore"],
+    ["steps.place_design.args.directive", "Explore"],
+    ["steps.place_design.args.more options", "-verbose"],
+    ["steps.phys_opt_design.args.directive", "AlternateFlowWithRetiming"],
+    ["steps.phys_opt_design.args.more options", "-verbose"],
+    ["steps.route_design.args.directive", "Explore"],
+    ["steps.route_design.args.more options", "-verbose"],
+    ["steps.post_route_phys_opt_design.args.directive", "AddRetime"],
+    ["steps.post_route_phys_opt_design.args.more options", "-verbose"],
+    ["steps.write_bitstream.args.verbose", "1"]]
 
 import os
 import sys
