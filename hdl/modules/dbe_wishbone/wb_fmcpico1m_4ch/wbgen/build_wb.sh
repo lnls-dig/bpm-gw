3f0f971f53454e86bc9fba896de22e34064c1bc0
Lucas Russo <lerwys@gmail.com>
2017-03-15 18:43:26 -0300

    modules/*/wb_fmcpico1m_4ch/*: renamed wbgen files/scripts
    This is just for consistency. No actual
    changes made.

--- hdl/modules/dbe_wishbone/wb_fmcpico1m_4ch/wbgen/build_wb.sh
+++ hdl/modules/dbe_wishbone/wb_fmcpico1m_4ch/wbgen/build_wb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wbgen2 -V fmc_pico1m_4ch_regs.vhd -H record -p fmc_pico1m_4ch_regs_pkg.vhd -K ../../../../sim/regs/wb_fmcpico1m_4ch_regs.vh -s defines -C fmcpico1m_4ch_regs.h -f html -D doc/fmcpico1m_4ch_regs_wb.html fmc_pico1m_4ch_regs.wb
+wbgen2 -V wb_fmcpico1m_4ch_regs.vhd -H record -p wb_fmcpico1m_4ch_regs_pkg.vhd -K ../../../../sim/regs/wb_fmcpico1m_4ch_regs.vh -s defines -C wb_fmcpico1m_4ch_regs.h -f html -D doc/fmcpico1m_4ch_regs_wb.html wb_fmcpico1m_4ch_regs.wb
