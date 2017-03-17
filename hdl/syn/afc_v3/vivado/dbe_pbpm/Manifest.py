e23b0631901cf759fafe7411317e66e01820c7fd
Lucas Russo <lerwys@gmail.com>
2017-03-17 20:55:20 -0300

    syn/*/dbe_pbpm: add additional implementation properties

--- hdl/syn/afc_v3/vivado/dbe_pbpm/Manifest.py
+++ hdl/syn/afc_v3/vivado/dbe_pbpm/Manifest.py
@@ -7,8 +7,29 @@ syn_package = "ffg1156"
 syn_top = "dbe_pbpm"
 syn_project = "dbe_pbpm"
 syn_tool = "vivado"
+syn_properties = [
+    ["steps.synth_design.args.more options", "-verbose"],
+    ["steps.opt_design.args.verbose", "1"],
+    ["steps.opt_design.args.directive", "Explore"],
+    ["steps.opt_design.is_enabled", "1"],
+    ["steps.place_design.args.directive", "Explore"],
+    ["steps.place_design.args.more options", "-verbose"],
+    ["steps.phys_opt_design.args.directive", "AlternateFlowWithRetiming"],
+    ["steps.phys_opt_design.args.more options", "-verbose"],
+    ["steps.phys_opt_design.is_enabled", "1"],
+    ["steps.route_design.args.directive", "Explore"],
+    ["steps.route_design.args.more options", "-verbose"],
+    ["steps.post_route_phys_opt_design.args.directive", "AddRetime"],
+    ["steps.post_route_phys_opt_design.args.more options", "-verbose"],
+    ["steps.post_route_phys_opt_design.is_enabled", "1"],
+    ["steps.write_bitstream.args.verbose", "1"]]
 
-files = ["synthesis_descriptor_pkg.vhd"];
+import os
+import sys
+if os.path.isfile("synthesis_descriptor_pkg.vhd"):
+    files = ["synthesis_descriptor_pkg.vhd"];
+else:
+    sys.exit("Generate the SDB descriptor before using HDLMake (./build_synthesis_sdb.sh)")
 
 # Doesn't matter got PBPM
 machine_pkg = "uvx_250M"
