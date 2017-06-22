180e2574843a3838cbda2ac0c2d894d748c1f338
Lucas Russo <lerwys@gmail.com>
2017-06-22 09:38:53 -0300

    syn/*/dbe_pbpm: change synthesis project name to pbpm-gw
    This is a more semantic name.

--- hdl/syn/afc_v3/vivado/dbe_pbpm/build_synthesis_sdb.sh
+++ hdl/syn/afc_v3/vivado/dbe_pbpm/build_synthesis_sdb.sh
@@ -5,7 +5,7 @@ set -e
 # Check for uninitialized variables
 set -u
 
-SYNTH_INFO_PROJECT="bpm-pbpm"
+SYNTH_INFO_PROJECT="pbpm-gw"
 SYNTH_INFO_TOOL="VIVADO"
 SYNTH_INFO_VER=$(vivado -version | head -n 1 | cut -d' ' -f2 | cut -d 'v' -f2)
 
