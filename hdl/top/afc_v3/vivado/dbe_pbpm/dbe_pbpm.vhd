86e98e3d52b171f8293853f455325254866bb4e0
Lucas Russo <lerwys@gmail.com>
2019-03-21 15:59:44 -0300

    top/afcv3/*: add missing aux_clk assignments to top-level

--- hdl/top/afc_v3/vivado/dbe_pbpm/dbe_pbpm.vhd
+++ hdl/top/afc_v3/vivado/dbe_pbpm/dbe_pbpm.vhd
@@ -35,6 +35,9 @@ port(
   sys_clk_p_i                                : in std_logic;
   sys_clk_n_i                                : in std_logic;
 
+  aux_clk_p_i                                : in std_logic;
+  aux_clk_n_i                                : in std_logic;
+
   -----------------------------------------
   -- Reset Button
   -----------------------------------------
@@ -198,6 +201,9 @@ architecture rtl of dbe_pbpm is
     -----------------------------------------
     sys_clk_p_i                                : in std_logic;
     sys_clk_n_i                                : in std_logic;
+
+    aux_clk_p_i                                : in std_logic;
+    aux_clk_n_i                                : in std_logic;
 
     -----------------------------------------
     -- Reset Button
@@ -692,6 +698,9 @@ begin
     -----------------------------------------
     sys_clk_p_i                               => sys_clk_p_i,
     sys_clk_n_i                               => sys_clk_n_i,
+
+    aux_clk_p_i                               => aux_clk_p_i,
+    aux_clk_n_i                               => aux_clk_n_i,
 
     -----------------------------------------
     -- Reset Button
