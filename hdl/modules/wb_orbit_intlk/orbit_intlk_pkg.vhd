d62e431abc0974d120bba04dbf8bb197b1b5f0cd
Lucas Russo <lerwys@gmail.com>
2020-06-19 12:35:48 -0300

    modules/wb_orbit_intlk: add missing component to package

--- hdl/modules/wb_orbit_intlk/orbit_intlk_pkg.vhd
+++ hdl/modules/wb_orbit_intlk/orbit_intlk_pkg.vhd
@@ -35,6 +35,120 @@ package orbit_intlk_pkg is
   --------------------------------------------------------------------
   -- Components
   --------------------------------------------------------------------
+  component orbit_intlk
+  generic
+  (
+    g_ADC_WIDTH                                : natural := 16;
+    g_DECIM_WIDTH                              : natural := 32;
+    -- interlock limits
+    g_INTLK_LMT_WIDTH                          : natural := 32
+  );
+  port
+  (
+    -----------------------------
+    -- Clocks and resets
+    -----------------------------
+
+    ref_rst_n_i                                : in std_logic;
+    ref_clk_i                                  : in std_logic;
+
+    -----------------------------
+    -- Interlock enable and limits signals
+    -----------------------------
+
+    intlk_en_i                                 : in std_logic;
+    intlk_clr_i                                : in std_logic;
+    -- Minimum threshold interlock on/off
+    intlk_min_sum_en_i                         : in std_logic;
+    -- Minimum threshold to interlock
+    intlk_min_sum_i                            : in std_logic_vector(g_INTLK_LMT_WIDTH-1 downto 0);
+    -- Translation interlock on/off
+    intlk_trans_en_i                           : in std_logic;
+    -- Translation interlock clear
+    intlk_trans_clr_i                          : in std_logic;
+    intlk_trans_max_x_i                        : in std_logic_vector(g_INTLK_LMT_WIDTH-1 downto 0);
+    intlk_trans_max_y_i                        : in std_logic_vector(g_INTLK_LMT_WIDTH-1 downto 0);
+    -- Angular interlock on/off
+    intlk_ang_en_i                             : in std_logic;
+    -- Angular interlock clear
+    intlk_ang_clr_i                            : in std_logic;
+    intlk_ang_max_x_i                          : in std_logic_vector(g_INTLK_LMT_WIDTH-1 downto 0);
+    intlk_ang_max_y_i                          : in std_logic_vector(g_INTLK_LMT_WIDTH-1 downto 0);
+
+    -----------------------------
+    -- Downstream ADC and position signals
+    -----------------------------
+
+    fs_clk_ds_i                                : in std_logic;
+
+    adc_ds_ch0_swap_i                          : in std_logic_vector(g_ADC_WIDTH-1 downto 0) := (others => '0');
+    adc_ds_ch1_swap_i                          : in std_logic_vector(g_ADC_WIDTH-1 downto 0) := (others => '0');
+    adc_ds_ch2_swap_i                          : in std_logic_vector(g_ADC_WIDTH-1 downto 0) := (others => '0');
+    adc_ds_ch3_swap_i                          : in std_logic_vector(g_ADC_WIDTH-1 downto 0) := (others => '0');
+    adc_ds_tag_i                               : in std_logic_vector(0 downto 0) := (others => '0');
+    adc_ds_swap_valid_i                        : in std_logic := '0';
+
+    decim_ds_pos_x_i                           : in std_logic_vector(g_DECIM_WIDTH-1 downto 0);
+    decim_ds_pos_y_i                           : in std_logic_vector(g_DECIM_WIDTH-1 downto 0);
+    decim_ds_pos_q_i                           : in std_logic_vector(g_DECIM_WIDTH-1 downto 0);
+    decim_ds_pos_sum_i                         : in std_logic_vector(g_DECIM_WIDTH-1 downto 0);
+    decim_ds_pos_valid_i                       : in std_logic;
+
+    -----------------------------
+    -- Upstream ADC and position signals
+    -----------------------------
+
+    fs_clk_us_i                                : in std_logic;
+
+    adc_us_ch0_swap_i                          : in std_logic_vector(g_ADC_WIDTH-1 downto 0) := (others => '0');
+    adc_us_ch1_swap_i                          : in std_logic_vector(g_ADC_WIDTH-1 downto 0) := (others => '0');
+    adc_us_ch2_swap_i                          : in std_logic_vector(g_ADC_WIDTH-1 downto 0) := (others => '0');
+    adc_us_ch3_swap_i                          : in std_logic_vector(g_ADC_WIDTH-1 downto 0) := (others => '0');
+    adc_us_tag_i                               : in std_logic_vector(0 downto 0) := (others => '0');
+    adc_us_swap_valid_i                        : in std_logic := '0';
+
+    decim_us_pos_x_i                           : in std_logic_vector(g_DECIM_WIDTH-1 downto 0);
+    decim_us_pos_y_i                           : in std_logic_vector(g_DECIM_WIDTH-1 downto 0);
+    decim_us_pos_q_i                           : in std_logic_vector(g_DECIM_WIDTH-1 downto 0);
+    decim_us_pos_sum_i                         : in std_logic_vector(g_DECIM_WIDTH-1 downto 0);
+    decim_us_pos_valid_i                       : in std_logic;
+
+    -----------------------------
+    -- Interlock outputs
+    -----------------------------
+    intlk_trans_bigger_x_o                     : out std_logic;
+    intlk_trans_bigger_y_o                     : out std_logic;
+
+    intlk_trans_bigger_ltc_x_o                 : out std_logic;
+    intlk_trans_bigger_ltc_y_o                 : out std_logic;
+
+    intlk_trans_bigger_o                       : out std_logic;
+
+    -- only cleared when intlk_trans_clr_i is asserted
+    intlk_trans_ltc_o                          : out std_logic;
+    -- conditional to intlk_trans_en_i
+    intlk_trans_o                              : out std_logic;
+
+    intlk_ang_bigger_x_o                       : out std_logic;
+    intlk_ang_bigger_y_o                       : out std_logic;
+
+    intlk_ang_bigger_ltc_x_o                   : out std_logic;
+    intlk_ang_bigger_ltc_y_o                   : out std_logic;
+
+    intlk_ang_bigger_o                         : out std_logic;
+
+    -- only cleared when intlk_ang_clr_i is asserted
+    intlk_ang_ltc_o                            : out std_logic;
+    -- conditional to intlk_ang_en_i
+    intlk_ang_o                                : out std_logic;
+
+    -- only cleared when intlk_clr_i is asserted
+    intlk_ltc_o                                : out std_logic;
+    -- conditional to intlk_en_i
+    intlk_o                                    : out std_logic
+  );
+  end component;
+
   component orbit_intlk_trans is
   generic
   (
