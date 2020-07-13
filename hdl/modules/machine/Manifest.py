3b297d6b0d85cc53a172ae5265ca6eafba1df40f
Lucas Russo <lerwys@gmail.com>
2020-07-13 16:28:40 -0300

    modules/machine: remove unused UVX machine parameters

--- hdl/modules/machine/Manifest.py
+++ hdl/modules/machine/Manifest.py
@@ -1,10 +1,4 @@
-# TODO: if hdlmake allowed for importing python packages in inner modules, this could existing directories directly
-
-if (machine_pkg == "uvx_sr_130M"):
-    modules = { "local" : "uvx_sr_130M"}
-elif (machine_pkg == "uvx_sr_250M"):
-    modules = { "local" : "uvx_sr_250M"}
-elif (machine_pkg == "sirius_sr_130M"):
+if (machine_pkg == "sirius_sr_130M"):
     modules = { "local" : "sirius_sr_130M"}
 elif (machine_pkg == "sirius_sr_250M"):
     modules = { "local" : "sirius_sr_250M"}
